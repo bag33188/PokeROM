1a5df7d4fd6e11ef38e4705cd754125a8c68b41b
bag33188 <shuckle168@gmail.com>
2019-07-04 15:02:15 -0700

    return desc

--- client/src/app/components/roms/rom-info/game-description/game-description.component.ts
+++ client/src/app/components/roms/rom-info/game-description/game-description.component.ts
@@ -23,10 +23,14 @@ export class GameDescriptionComponent implements OnInit {
 
   changeUrlToLink(description: string): string {
     const urlRegex: RegExp = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
-    const url: string = description.match(urlRegex)[0];
-    return description.replace(
-      url,
-      `<a href="${url}" target="_blank">${url}</a>`
-    );
+    if (urlRegex.test(description)) {
+      const url: string = description.match(urlRegex)[0];
+      return description.replace(
+        url,
+        `<a href="${url}" target="_blank">${url}</a>`
+      );
+    } else {
+      return description;
+    }
   }
 }
