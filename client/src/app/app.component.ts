6ed483c9b41ffa352c4f11814c51606152a9a7aa
bag33188 <shuckle168@gmail.com>
2019-07-05 15:30:27 -0700

    add html comments

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,11 +6,13 @@ import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   template: `
+    <!-- begin page container/wrapper -->
     <div attr.data-name="{{ title }}" id="container-wrapper">
       <app-header></app-header>
       <app-body></app-body>
       <app-footer [appName]="title"></app-footer>
     </div>
+    <!-- end page container/wrapper -->
   `,
   styles: [
     `
