3ef89cdfcadec36046f8a2bba4ee2f649dcb9cce
bag33188 <shuckle168@gmail.com>
2019-07-05 15:20:31 -0700

    fix img object cover scaling

--- client/src/app/components/roms/rom/rom.component.ts
+++ client/src/app/components/roms/rom/rom.component.ts
@@ -14,6 +14,22 @@ export class RomComponent implements OnInit {
   ngOnInit() {}
 
   imgAlt(altValue: string): string {
-    return `${altValue.replace(/[\s:]/g, '-').replace(/\xe9/g, 'e')}-box-art`;
+    return `${altValue
+      .replace(/[\s:]/g, '-')
+      .replace(/\xe9/g, 'e')
+      .replace('\'', '')}-box-art`;
+  }
+
+  /**
+   * @summary Checks the Pokemon Game.
+   * @description Checks if the game is Pokemon Green, Pokemon Let's Go Pikachu, and/or Pokemon Lets Go Eevee.
+   * @param altValue The alt value of the image.
+   */
+  checkGame(altValue: string): string {
+    if (/pokemon\-(green\-version|lets-go)/i.test(altValue)) {
+      return 'oversized-img card-img-top box-art-img';
+    } else {
+      return 'card-img-top box-art-img';
+    }
   }
 }
