98e5830d37a1a646b36f34ed848ff4cc22eb76b2
bag33188 <shuckle168@gmail.com>
2019-07-10 09:48:12 -0700

    fix url regex

--- client/src/app/components/roms/rom-info/game-description/game-description.component.ts
+++ client/src/app/components/roms/rom-info/game-description/game-description.component.ts
@@ -22,7 +22,7 @@ export class GameDescriptionComponent implements OnInit {
   }
 
   changeUrlToLink(description: string): string {
-    const urlRegex: RegExp = /^(?:[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))$/i;
+    const urlRegex: RegExp = /(?:[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))/i;
     if (urlRegex.test(description)) {
       const url: string = description.match(urlRegex)[0];
       return description.replace(
