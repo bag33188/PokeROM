c92f79db70083f53e48dbdfa6da06483e32f24ad
bag33188 <shuckle168@gmail.com>
2019-07-10 05:42:56 -0700

    add additonal cors config

--- config/cors.js
+++ config/cors.js
@@ -1,11 +1,32 @@
 const cors = require('cors');
 
 module.exports = cors({
+  credentials: true,
+  exposedHeaders: [
+    'Cache-Control',
+    'Content-Language',
+    'Content-Type',
+    'Expires',
+    'Last-Modified',
+    'Pragma',
+    'Content-Range',
+    'X-Content-Range'
+  ],
+  headers: [
+    'Accept',
+    'Accept-Language',
+    'Authorization',
+    'Content-Type',
+    'Content-Language',
+    'Save-Data',
+    'Viewport-Width',
+    'Width'
+  ],
+  maxAge: 86400,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  optionsSuccessStatus: 204,
   origin:
     process.env.NODE_ENV === 'production'
       ? 'https://pokerom-broccolini.herokuapp.com'
-      : 'http://localhost:4200',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-  headers: ['Accept', 'Accept-Language', 'Authorization', 'Content-Type'],
-  credentials: true
+      : 'http://localhost:4200'
 });
