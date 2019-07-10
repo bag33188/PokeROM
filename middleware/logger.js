740c58ac71fdcc7810ff283301607c65f566ed59
bag33188 <shuckle168@gmail.com>
2019-07-10 11:57:56 -0700

    update logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -9,7 +9,7 @@ const moment = require('moment');
  */
 function logger(req, res, next) {
   const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-  console.log(`${req.method} ${url} ${moment().format()}`);
+  console.log(`${req.method} ${url} ${moment().format('LLLL')}`);
   next();
 }
 
