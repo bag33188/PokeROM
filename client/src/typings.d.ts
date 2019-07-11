f09fcf8a21b8c03219609d707d2346acd3efb364
bag33188 <shuckle168@gmail.com>
2019-07-11 15:24:45 -0700

    update module resolution typings

--- client/src/typings.d.ts
+++ client/src/typings.d.ts
@@ -1,7 +1,35 @@
 /* CommonJS module definition */
 declare var module: NodeModule;
-interface NodeModule {
+
+/* CommonJS module interfaces */
+interface NodeRequireFunction {
   (id: string): any;
+}
+interface NodeRequire extends NodeRequireFunction {
+  resolve: RequireResolve;
+  cache: any;
+  extensions: NodeExtensions;
+  main: NodeModule | undefined;
+}
+interface RequireResolve {
+  (id: string, options?: { paths?: string[] }): string;
+  paths(request: string): string[] | null;
+}
+interface NodeExtensions {
+  '.js': (m: NodeModule, filename: string) => any;
+  '.json': (m: NodeModule, filename: string) => any;
+  '.node': (m: NodeModule, filename: string) => any;
+  [ext: string]: (m: NodeModule, filename: string) => any;
+}
+interface NodeModule {
+  exports: any;
+  require: NodeRequireFunction;
+  id: string;
+  filename: string;
+  loaded: boolean;
+  parent: NodeModule | null;
+  children: NodeModule[];
+  paths: string[];
 }
 
 /* Module Delcarations */
