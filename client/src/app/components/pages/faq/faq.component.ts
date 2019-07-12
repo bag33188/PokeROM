de3d9944a770f922f71f5dd6519fc1928560014c
bag33188 <shuckle168@gmail.com>
2019-07-12 16:57:27 -0700

    update fragment work

--- client/src/app/components/pages/faq/faq.component.ts
+++ client/src/app/components/pages/faq/faq.component.ts
@@ -1,25 +1,24 @@
-import { Component, OnInit, AfterContentInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import he from 'he';
-import LoggedUser from '../../../models/LoggedUser';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.scss']
 })
-export class FaqComponent implements OnInit, AfterContentInit {
+export class FaqComponent implements OnInit {
   emulatorUrl: string;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     this.emulatorUrl = 'https://www.retroarch.com';
-  }
-
-  ngAfterContentInit() {
     if (!this.route.snapshot.fragment) {
       window.scrollTo(0, 0);
+    } else {
+      $('#' + this.route.snapshot.fragment)[0].scrollIntoView();
+      // window.location.hash = this.route.snapshot.fragment;
     }
   }
 }
