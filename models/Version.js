7c9e0f7900620f33b42dd087fee44700d26803d3
bag33188 <shuckle168@gmail.com>
2019-07-12 12:40:08 -0700

    add models for api data

---
+++ models/Version.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const uuid = require('uuid');
+
+/*
+ * uuid.v1()
+ * uuid.v4() 
+ */
+
+// create mongoose schema
+const VersionSchema = mongoose.Schema({
+  apiVersion: String
+});
+
+// create version model
+const Version = module.exports = mongoose.model('Version', VersionSchema);
+
+/**
+ * @summary Get API Version
+ * @description Gets the API's version.
+ * @param {string} apiVersion The API's version.
+ * @returns {Version} Version object.
+ */
+module.exports.getApiVersion = (apiVersion) => {
+  const version = new Version({ apiVersion });
+  return version;
+};
