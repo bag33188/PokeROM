36c2b022422e42e7e32c9d139ec62be1f741ecf1
bag33188 <shuckle168@gmail.com>
2019-07-12 13:22:31 -0700

    add external enums

---
+++ client/src/app/enums/images.enum.ts
@@ -0,0 +1,7 @@
+enum Images {
+  IMG0 = '/assets/images/carousel_img_00.png',
+  IMG1 = '/assets/images/carousel_img_01.png',
+  IMG2 = '/assets/images/carousel_img_02.png',
+  IMG3 = '/assets/images/carousel_img_03.png'
+}
+export default Images;
