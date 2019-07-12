be96d8f7a30f28ef8b2ec28537675c45ae3d5193
bag33188 <shuckle168@gmail.com>
2019-07-12 12:39:56 -0700

    add api middleware

---
+++ middleware/logger.js
@@ -0,0 +1,29 @@
+const moment = require('moment');
+
+/**
+ * @summary Log request.
+ * @description Logs the request with the exact date and time
+ * @param {object} req JSON request body.
+ * @param {object} res JSON response.
+ * @param {any} next Next callback.
+ */
+function logger(req, res, next) {
+  const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+  console.log(
+    `${req.method} ${url} ${moment()
+      .subtract(7, 'hours') // pacific standard time
+      .format('LLLL')}`
+  );
+  next();
+}
+
+/*
+function findOffset() {
+  const now = new Date().getHours();
+  const utc = new Date().getUTCHours();
+  return utc - now;
+}
+console.log(findOffset());
+*/
+
+module.exports = logger;
