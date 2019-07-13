8c7eef72311898c6243054c25dc24f2c4c4d49db
bag33188 <shuckle168@gmail.com>
2019-07-12 17:20:45 -0700

    update production script

--- tests/production.sh
+++ tests/production.sh
@@ -1,44 +1,47 @@
 #!/bin/bash
 
-cd ../client
-ng build --prod && (
-  cd ../public
-  if [ -f "*.html" ]; then
-    rm *.html
-  fi
-  if [ -f "*.css" ]; then
-    rm *.css
-  fi
-  if [ -f "*js" ]; then
-    rm *js
-  fi
-  if [ -f "*.ico" ]; then
-    rm *.ico
-  fi
-  if [ -f "*.txt" ]; then
-    rm *.txt
-  fi
-  if [ -d "assets" ]; then
-    rm -rf assets
-  fi
-  mv ../client/dist/pokerom/*.* ./
-  mv ../client/dist/pokerom/assets ./
-  cd ..
-  rm -rf client/dist
-  while [ true ]
-  do
-    git status
-    read -p "Add files: " files
-    git add $files
-    read -p "Commit Message: " commit_msg
-    git commit -m "$commit_msg"
-    read -p "Done? (y/n) " is_done
-    if [[ $is_done == 'y' ]]; then
-      break
-    else
-      continue
+production() {
+  cd ../client
+  ng build --prod && (
+    cd ../public
+    if [ -f "*.html" ]; then
+      rm *.html
     fi
-  done
-  git push
-  git push heroku master
-) && read -n 1 -s -r -p "Press any key to exit" && echo -e "\r\n"
+    if [ -f "*.css" ]; then
+      rm *.css
+    fi
+    # if [ -f "*js" ]; then
+    rm *js
+    # fi
+    if [ -f "*.ico" ]; then
+      rm *.ico
+    fi
+    if [ -f "*.txt" ]; then
+      rm *.txt
+    fi
+    if [ -d "assets" ]; then
+      rm -rf assets
+    fi
+    mv ../client/dist/pokerom/*.* ./
+    mv ../client/dist/pokerom/assets ./
+    cd ..
+    rm -rf client/dist
+    while [ true ]
+    do
+      git status
+      read -p "Add files: " files
+      git add $files
+      read -p "Commit Message: " commit_msg
+      git commit -m "$commit_msg"
+      read -p "Done? (y/n) " is_done
+      if [[ $is_done == 'y' ]]; then
+        break
+      else
+        continue
+      fi
+    done
+    git push
+    git push heroku master
+  ) && read -n 1 -s -r -p && echo -e "\r\n" && "Press any key to exit" && echo -e "\r\n" 
+}
+production
