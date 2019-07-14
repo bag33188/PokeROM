138c54563baa1e6de30abffcbc84d4f6e619a21a
bag33188 <shuckle168@gmail.com>
2019-07-14 12:44:41 -0700

    remove cookie methods

--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -14,43 +14,4 @@ export class ApiService {
     const apiVersion: Promise<Response> = axios.get(this.apiVersionUrl);
     return apiVersion;
   }
-
-  public getCookie(cookieName: string): string {
-    const name: string = `${cookieName}=`;
-    const decodedCookie: string = decodeURIComponent(document.cookie);
-    const cookieArray: string[] = decodedCookie.split(';');
-    for (const cookieData of cookieArray) {
-      let cookie: string = cookieData;
-      while (cookie.charAt(0) === ' ') {
-        cookie = cookie.substring(1);
-      }
-      if (cookie.indexOf(name) === 0) {
-        return cookie.substring(name.length, cookie.length);
-      }
-    }
-    return '';
-  }
-
-  public setCookie(
-    cookieName: string,
-    cookieValue: string,
-    expireDays: number
-  ): void {
-    const now: Date = new Date();
-    now.setTime(now.getTime() + expireDays * 24 * 60 * 60 * 1000);
-    const expires: string = `expires=${now.toUTCString()}`;
-    document.cookie = `${cookieName}=${cookieValue};${expires};path=/`;
-  }
-
-  public checkCookie(cookieName: string, expireDays: number, callbacks: any[]): void {
-    const cookie: string = this.getCookie(cookieName);
-    if (cookie !== '') {
-      callbacks[0]();
-    } else {
-      callbacks[1]();
-      if (cookie !== '' && cookie != null) {
-        this.setCookie(cookieName, cookie, expireDays);
-      }
-    }
-  }
 }
