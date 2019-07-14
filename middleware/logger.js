f676afc0e295921913ac3d47a8930f8443ada312
bag33188 <shuckle168@gmail.com>
2019-07-14 10:57:25 -0700

    update logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -9,11 +9,15 @@ const moment = require('moment');
  */
 function logger(req, res, next) {
   const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-  console.log(
-    `${req.method} ${url} ${moment()
-      .subtract(7, 'hours') // pacific standard time
-      .format('LLLL')}`
-  );
+  if (process.env.NODE_ENV === 'production') {
+    console.log(
+      `${req.method} ${url} ${moment()
+        .subtract(7, 'hours') // pacific standard time
+        .format('LLLL')}`
+    );
+  } else {
+    console.log(`${req.method} ${url} ${moment().format('LLLL')}`);
+  }
   next();
 }
 
