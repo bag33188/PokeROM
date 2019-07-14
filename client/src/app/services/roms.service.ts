ee4a8a51930e77f8c1bfd34142ff889d600c8274
bag33188 <shuckle168@gmail.com>
2019-07-14 12:45:08 -0700

    change api to cookie injection

--- client/src/app/services/roms.service.ts
+++ client/src/app/services/roms.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import Rom from '../models/Rom';
 import { environment } from '../../environments/environment';
-import { ApiService } from './api.service';
+import { CookiesService } from './cookies.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +12,11 @@ import { ApiService } from './api.service';
 export class RomsService {
   private romsUrl: string = `${environment.apiUrl}/api/roms`;
 
-  constructor(private http: HttpClient, private apiService: ApiService) {}
+  constructor(private http: HttpClient, private cookieService: CookiesService) {}
 
   public getAllRoms(limit?: number): Observable<Rom[]> {
     const headers: HttpHeaders = new HttpHeaders({
-      Authorization: this.apiService.getCookie('token_id')
+      Authorization: this.cookieService.getCookie('token_id')
     });
     if (limit) {
       return this.http.get<Rom[]>(`${this.romsUrl}?_limit=${limit}`, {
@@ -31,21 +31,21 @@ export class RomsService {
 
   public getRom(id: string): Observable<Rom> {
     const headers: HttpHeaders = new HttpHeaders({
-      Authorization: this.apiService.getCookie('token_id')
+      Authorization: this.cookieService.getCookie('token_id')
     });
     return this.http.get<Rom>(`${this.romsUrl}/${id}`, { headers });
   }
 
   public addRom(rom: Rom): Observable<Rom> {
     const headers: HttpHeaders = new HttpHeaders({
-      Authorization: this.apiService.getCookie('token_id'),
+      Authorization: this.cookieService.getCookie('token_id'),
       'Content-Type': 'application/json'
     });
     return this.http.post<Rom>(this.romsUrl, rom, { headers });
   }
   public updateRom(id: string, rom: Rom): Observable<Rom> {
     const headers: HttpHeaders = new HttpHeaders({
-      Authorization: this.apiService.getCookie('token_id'),
+      Authorization: this.cookieService.getCookie('token_id'),
       'Content-Type': 'application/json'
     });
     return this.http.put<Rom>(`${this.romsUrl}/${id}`, rom, { headers });
@@ -53,7 +53,7 @@ export class RomsService {
 
   public patchRom(id: string, partialRom: any): Observable<Rom> {
     const headers: HttpHeaders = new HttpHeaders({
-      Authorization: this.apiService.getCookie('token_id'),
+      Authorization: this.cookieService.getCookie('token_id'),
       'Content-Type': 'application/json'
     });
     return this.http.patch<Rom>(`$${this.romsUrl}/${id}`, partialRom, {
@@ -63,26 +63,26 @@ export class RomsService {
 
   public deleteRom(id: string): Observable<any> {
     const headers: HttpHeaders = new HttpHeaders({
-      Authorization: this.apiService.getCookie('token_id')
+      Authorization: this.cookieService.getCookie('token_id')
     });
     return this.http.delete<any>(`$${this.romsUrl}/${id}`, { headers });
   }
   public deleteAllRoms(): Observable<any> {
     const headers: HttpHeaders = new HttpHeaders({
-      Authorization: this.apiService.getCookie('token_id')
+      Authorization: this.cookieService.getCookie('token_id')
     });
     return this.http.delete<any>(this.romsUrl, { headers });
   }
 
   public getHeadersAll(): Observable<void> {
     const headers: HttpHeaders = new HttpHeaders({
-      Authorization: this.apiService.getCookie('token_id')
+      Authorization: this.cookieService.getCookie('token_id')
     });
     return this.http.head<void>(this.romsUrl, { headers });
   }
   public getHeadersSingle(id: string): Observable<void> {
     const headers: HttpHeaders = new HttpHeaders({
-      Authorization: this.apiService.getCookie('token_id')
+      Authorization: this.cookieService.getCookie('token_id')
     });
     return this.http.head<void>(`${this.romsUrl}/${id}`, { headers });
   }
