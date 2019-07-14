570d45cec1866344c2b669bc08d56a5b45338b52
bag33188 <shuckle168@gmail.com>
2019-07-14 10:11:09 -0700

    update static files

--- client/src/app/components/pages/faq/faq.component.ts
+++ client/src/app/components/pages/faq/faq.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import * as $ from 'jquery';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-faq',
