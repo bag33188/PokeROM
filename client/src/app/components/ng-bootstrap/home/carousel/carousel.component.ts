ccc9bb83b573e56e2df53f216c2744ea4cc1e35a
bag33188 <shuckle168@gmail.com>
2019-07-15 01:47:36 -0700

    update carousel obj

--- client/src/app/components/ng-bootstrap/home/carousel/carousel.component.ts
+++ client/src/app/components/ng-bootstrap/home/carousel/carousel.component.ts
@@ -18,20 +18,20 @@ export class CarouselComponent implements OnInit {
     this.images = Images;
     this.carouselData = [
       {
-        img: this.images.IMG1,
+        image: this.images.IMG1,
         heading: he.decode('Now Introducing a New Pok&eacute;mon ROMs Website'),
-        details: he.decode('For All Core Pok&eacute;mon Games')
+        caption: he.decode('For All Core Pok&eacute;mon Games')
       },
       {
-        img: this.images.IMG2,
-        heading: `Including Let's Go Games`,
-        details: he.decode(`This database includes ROMs for Pok&eacute;mon Let's Go Pikachu &amp;
+        image: this.images.IMG2,
+        heading: he.decode(`Including Pok&eacute;mon Let's Go Games`),
+        caption: he.decode(`This database includes ROMs for Pok&eacute;mon Let's Go Pikachu &amp;
         Eevee`)
       },
       {
-        img: this.images.IMG3,
+        image: this.images.IMG3,
         heading: 'Now with ROM Hacks',
-        details: he.decode(
+        caption: he.decode(
           'You can now play your favorite Pok&eacute;mon ROM hacks'
         )
       }
