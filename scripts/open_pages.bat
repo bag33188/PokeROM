6f8fe31e86c77aeaef709b8d72f94f24495e1ff1
bag33188 <shuckle168@gmail.com>
2019-07-18 15:13:03 -0700

    add scripts folder

---
+++ scripts/open_pages.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+:OPEN_PAGES
+  START "" https://github.com/bag33188/PokeROM
+  START "" https://github.com/bag33188/PokeROM-Setup
+  START "" https://pokerom-broccolini.herokuapp.com
+  START "" https://cloud.mongodb.com
+  CD ..
+EXIT /B 0
