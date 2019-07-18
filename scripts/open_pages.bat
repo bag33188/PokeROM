d82c3f2e6c6f01f480e425f77130509bd440d58a
bag33188 <shuckle168@gmail.com>
2019-07-18 16:33:44 -0700

    update bat file

--- scripts/open_pages.bat
+++ scripts/open_pages.bat
@@ -1,9 +1,14 @@
 @ECHO OFF
 
 :OPEN_PAGES
+  REM PokeROM Github page
   START "" https://github.com/bag33188/PokeROM
+  REM PokeROM Setup Github page
   START "" https://github.com/bag33188/PokeROM-Setup
+  REM PokeROM Website (Heroku App)
   START "" https://pokerom-broccolini.herokuapp.com
+  REM MongoDB Atlas Cloud
   START "" https://cloud.mongodb.com
+  :: Go up one directory
   CD ..
 EXIT /B 0
