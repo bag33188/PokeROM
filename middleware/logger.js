7e1a1d3207916a87ae7e5effeab3b2fe902da547
bag33188 <bglatman@callutheran.edu>
2019-07-22 16:28:17 -0700

    add expressjs middleware

---
+++ middleware/logger.js
@@ -0,0 +1,36 @@
+const moment = require('moment');
+
+/**
+ * @summary Log request.
+ * @description Logs the request with the exact date and time
+ * @param {object} req JSON request body.
+ * @param {object} res JSON response.
+ * @param {any} next Next callback.
+ */
+function logger(req, res, next) {
+  const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+  if (process.env.NODE_ENV === 'production') {
+    console.log(
+      `${req.method} ${url} ${moment()
+        .subtract(7, 'hours') // pacific standard time
+        .format('LLLL')}`
+    );
+  } else {
+    console.log(`${req.method} ${url} ${moment().format('LLLL')}`);
+  }
+  next();
+}
+
+function findOffset() {
+  const nowLocal = new Date();
+  const nowUtc = new Date();
+  const localHours = nowLocal.getHours();
+  const utcHours = nowUtc.getUTCHours();
+  return utcHours - localHours;
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log(`Logger time offset: ${findOffset()}`);
+}
+
+module.exports = logger;
