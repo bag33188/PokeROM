2b80871c3caf85b71c08fa1fa962f44c32007a0c
bag33188 <bglatman@callutheran.edu>
2019-07-22 16:26:50 -0700

    add db and api config files

---
+++ config/cors.js
@@ -0,0 +1,38 @@
+const cors = require('cors');
+
+const corsConfig = cors({
+  credentials: true,
+  exposedHeaders: [
+    'Allow',
+    'Cache-Control',
+    'Content-Language',
+    'Content-Type',
+    'Expires',
+    'Last-Modified',
+    'Pragma',
+    'Content-Range',
+    'X-Content-Range',
+    'X-Requested-With'
+  ],
+  headers: [
+    'Accept',
+    'Accept-Language',
+    'Allow',
+    'Authorization',
+    'Content-Type',
+    'Content-Language',
+    'Save-Data',
+    'Viewport-Width',
+    'Width',
+    'X-Requested-With'
+  ],
+  maxAge: 86400,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  optionsSuccessStatus: 204,
+  origin:
+    process.env.NODE_ENV === 'production'
+      ? 'https://pokerom-broccolini.herokuapp.com'
+      : 'http://localhost:4200'
+});
+
+module.exports = corsConfig;
