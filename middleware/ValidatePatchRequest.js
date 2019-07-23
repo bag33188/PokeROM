4d59a0f15e770b4b478156bf8c285b206e9a8df2
bag33188 <bglatman@callutheran.edu>
2019-07-23 14:28:52 -0700

    update patch checking

--- middleware/ValidatePatchRequest.js
+++ middleware/ValidatePatchRequest.js
@@ -1,4 +1,5 @@
 class ValidatePatchRequest {
+
   constructor(req) {
     this.req = req;
   }
@@ -12,8 +13,8 @@ class ValidatePatchRequest {
         return res.status(406).json({success: false, message: 'Order number must be an integer from 0 to 88.'});
       }
     }
-    if (this.req.body.fileName) {
-      if (this.req.body.fileName.length < 3 || this.req.body.fileName.length > 80) {
+    if (this.req.body.fileName || this.req.body.fileName === '') {
+      if (this.req.body.fileName.length < 3 || this.req.body.fileName.length > 80 || this.req.body.fileName === '') {
         return res.status(406).json({success: false, message: 'File name must be between 3 and 80 characters.'});
       }
       if (typeof this.req.body.fileName !== typeof 'string') {
@@ -31,7 +32,7 @@ class ValidatePatchRequest {
         });
       }
     }
-    if (this.req.body.fileType) {
+    if (this.req.body.fileType || this.req.body.fileType === '') {
       if (!/^[a-zA-Z0-9]*$/.test(this.req.body.fileType)) {
         return res.status(406).json({success: false, message: 'File type must be alphanumeric.'});
       }
@@ -45,7 +46,7 @@ class ValidatePatchRequest {
         return res.status(406).json({success: false, message: 'File name must be a string'});
       }
     }
-    if (this.req.body.downloadLink) {
+    if (this.req.body.downloadLink || this.req.body.downloadLink === '') {
       if (!/^(?:[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))$/i.test(this.req.body.downloadLink)) {
         return res.status(406).json({success: false, message: 'Invalid URL.'});
       }
@@ -58,12 +59,12 @@ class ValidatePatchRequest {
         return res.status(406).json({success: false, message: 'Generation must be in between 1 and 8'});
       }
     }
-    if (this.req.body.boxArtUrl) {
+    if (this.req.body.boxArtUrl || this.req.body.boxArtUrl === '') {
       if (!/^(?:[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))$/i.test(this.req.body.boxArtUrl)) {
         return res.status(406).json({success: false, message: 'Invalid URL.'});
       }
     }
-    if (this.req.body.gameName) {
+    if (this.req.body.gameName || this.req.body.gameName === '') {
       if (typeof this.req.body.gameName !== typeof 'string') {
         return res.status(406).json({success: false, message: 'Game name must be a string'});
       }
@@ -71,7 +72,7 @@ class ValidatePatchRequest {
         return res.status(406).json({success: false, message: 'Game name must be in between 2 and 56 characters..'});
       }
     }
-    if (this.req.body.region) {
+    if (this.req.body.region || this.req.body.region === '') {
       if (!/^[a-zA-Z]$/.test(this.req.body.region)) {
         return res.status(406).json({success: false, message: 'Region must only contain alpha characters.'});
       }
@@ -79,7 +80,7 @@ class ValidatePatchRequest {
         return res.status(406).json({success: false, message: 'Region must be between 3 and 10 characters.'});
       }
     }
-    if (this.req.body.platform) {
+    if (this.req.body.platform || this.req.body.platform === '') {
       if (typeof this.req.body.platform !== typeof 'string') {
         return res.status(406).json({success: false, message: 'Platform must be a string.'});
       }
@@ -87,7 +88,7 @@ class ValidatePatchRequest {
         return res.status(406).json({success: false, message: 'Platform must be between 2 and 50 characters.'});
       }
     }
-    if (this.req.body.genre) {
+    if (this.req.body.genre  || this.req.body.genre === '') {
       if (typeof this.req.body.genre !== typeof 'string') {
         return res.status(406).json({success: false, message: 'Genre must be a string.'});
       }
@@ -95,17 +96,17 @@ class ValidatePatchRequest {
         return res.status(406).json({success: false, message: 'Genre must be less than 20 characters.'});
       }
     }
-    if (this.req.body.logoUrl) {
+    if (this.req.body.logoUrl || this.req.body.logoUrl === '') {
       if (!/^(?:[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))$/i.test(this.req.body.logoUrl)) {
         return res.status(406).json({success: false, message: 'Invalid URL.'});
       }
     }
-    if (this.req.body.dateReleased) {
+    if (this.req.body.dateReleased || this.req.body.dateReleased === '') {
       if (!/^(?:(0[1-9]|1[012])(\/|(&#x2[Ff];))(0[1-9]|[12][0-9]|3[01])(\/|(&#x2[Ff];))(\d{4}))$/.test(this.req.body.dateReleased)) {
         return res.status(406).json({success: false, message: 'Invalid URL.'});
       }
     }
-    if (this.req.body.description) {
+    if (this.req.body.description || this.req.body.description === '') {
       if (typeof this.req.body.description !== typeof 'string') {
         return res.status(406).json({success: false, message: 'Description must be a string'});
       }
@@ -116,7 +117,7 @@ class ValidatePatchRequest {
   }
 
   validateNaturePatch(res) {
-    if (this.req.body.usage) {
+    if (this.req.body.usage || this.req.body.usage === '') {
       if (typeof this.req.body.usage !== typeof 'string') {
         return res.status(406).json({success: false, message: 'Usage must be a string.'});
       }
@@ -124,7 +125,7 @@ class ValidatePatchRequest {
         return res.status(406).json({success: false, message: 'The usage for the nature must be in between 5 and 40 characters.'});
       }
     }
-    if (this.req.body.flavor) {
+    if (this.req.body.flavor || this.req.body.flavor === '') {
       if (typeof this.req.body.flavor !== typeof 'string') {
         return res.status(406).json({success: false, message: 'flavor must be a string.'});
       }
@@ -132,7 +133,7 @@ class ValidatePatchRequest {
         return res.status(406).json({success: false, message: 'The flavor for the nature must be greater than 14 characters.'});
       }
     }
-    if (this.req.body.down) {
+    if (this.req.body.down || this.req.body.down === '') {
       if (typeof this.req.body.down !== typeof 'string') {
         return res.status(406).json({ success: false, message: 'Down must be a string.' });
       }
@@ -140,7 +141,7 @@ class ValidatePatchRequest {
         return res.status(406).json({success: false, message: 'The decreased stat of the nature must be between 4 and 20 characters.'});
       }
     }
-    if (this.req.body.up) {
+    if (this.req.body.up || this.req.body.up === '') {
       if (typeof this.req.body.up !== typeof 'string') {
         return res.status(406).json({ success: false, message: 'Up must be a string.' });
       }
@@ -148,7 +149,7 @@ class ValidatePatchRequest {
         return res.status(406).json({success: false, message: 'The increased stat of the nature must be between 4 and 20 characters.'});
       }
     }
-    if (this.req.body.name) {
+    if (this.req.body.name || this.req.body.name === '') {
       if (typeof this.req.body.name !== typeof 'string') {
         return res.status(406).json({success: false, message: 'Name must be a string.'});
       }
@@ -159,7 +160,7 @@ class ValidatePatchRequest {
   }
 
   validateUserPatch(res) {
-    if (this.req.body.password) {
+    if (this.req.body.password || this.req.body.password === '') {
       if (typeof this.req.body.password !== typeof 'string') {
         return res.status(406).json({ success: false, message: 'Password must be a string.' });
       }
@@ -170,7 +171,7 @@ class ValidatePatchRequest {
         return res.status(406).json({ success: false, message: 'Password contains invalid characters.' });
       }
     }
-    if (this.req.body.username) {
+    if (this.req.body.username || this.req.body.username === '') {
       if (typeof this.req.body.username !== typeof 'string') {
         return res.status(406).json({ success: false, message: 'Username must be a string' });
       }
@@ -181,12 +182,12 @@ class ValidatePatchRequest {
         return res.status(406).json({ success: false, message: 'Username must be between 5 and 22 characters.' });
       }
     }
-    if (this.req.body.email) {
+    if (this.req.body.email || this.req.body.email === '') {
       if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.req.body.email)) {
         return res.status(406).json({ success: false, message: 'Invalid email.' });
       }
     }
-    if (this.req.body.name) {
+    if (this.req.body.name || this.req.body.name === '') {
       if (typeof this.req.body.name !== typeof 'string') {
         return res.status(406).json({ success: false, message: 'Name must be a string' });
       }
