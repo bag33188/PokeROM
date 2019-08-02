67693976a200a3697924d0aa1d0acc4f44b39fe0
bag33188 <shuckle168@gmail.com>
2019-08-02 00:49:38 -0700

    fix

--- client/src/app/guards/is-secure.guard.ts
+++ client/src/app/guards/is-secure.guard.ts
@@ -7,9 +7,9 @@ import { CanActivate, ActivatedRouteSnapshot } from '@angular/router';
 export class IsSecureGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot): boolean {
     if (!isDevMode() && location.protocol !== 'https:') {
-      location.href =
-        'https:' +
-        window.location.href.substring(window.location.protocol.length);
+      location.href = `https:${window.location.href.substring(
+        window.location.protocol.length
+      )}`;
       return false;
     }
     return true;
