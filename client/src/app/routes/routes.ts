0c21b42a8539ad4807cf9d8a2e65c5c4a5b32e6c
bag33188 <shuckle168@gmail.com>
2019-08-03 14:21:45 -0700

    remove route

--- client/src/app/routes/routes.ts
+++ client/src/app/routes/routes.ts
@@ -7,7 +7,7 @@ import { RomsComponent } from '../components/pages/roms/roms.component';
 import { RomInfoComponent } from '../components/pages/rom-info/rom-info.component';
 import { FaqComponent } from '../components/pages/faq/faq.component';
 import { NaturesComponent } from '../components/pages/natures/natures.component';
-import { DocsComponent } from '../components/pages/docs/docs.component';
+// import { DocsComponent } from '../components/pages/docs/docs.component';
 import { RatingsComponent } from '../components/pages/ratings/ratings.component';
 import { IsSecureGuard } from '../guards/is-secure.guard';
 import { RegisterComponent } from '../components/pages/register/register.component';
@@ -47,7 +47,7 @@ export const routes: Routes = [
     component: RatingsComponent,
     canActivate: [IsSecureGuard]
   },
-  { path: 'docs', component: DocsComponent, canActivate: [IsSecureGuard] },
+  // { path: 'docs', component: DocsComponent, canActivate: [IsSecureGuard] },
   { path: '404', component: NotFoundComponent, canActivate: [IsSecureGuard] },
   { path: '**', component: NotFoundComponent, canActivate: [IsSecureGuard] }
   // { path: '**', redirectTo: '/404', canActivate: [IsSecureGuard] },
