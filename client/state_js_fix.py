de844c5f011a135035862df3d9f6128aaa4857fe
bag33188 <shuckle168@gmail.com>
2019-08-05 08:12:19 -0700

    fix

--- client/state_js_fix.py
+++ client/state_js_fix.py
@@ -2,13 +2,21 @@
 
 import fileinput
 
-def fix_state_js():
-  filename = './node_modules/@angular/compiler-cli/src/ngtsc/incremental/src/state.js'
-  text_to_search = 'if (this.modifiedResourceFiles === undefined || !this.metadata.has(sf))'
-  replacement_text = 'if (this.modifiedResourceFiles === undefined || this.modifiedResourceFiles === null || !this.metadata.has(sf))'
+class AngularBug:
 
-  with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
-    for line in file:
-      print(line.replace(text_to_search, replacement_text), end='')
+  def __init__(self):
+    print('Fixing state.js...')
 
-fix_state_js()
+  @staticmethod
+  def fix_state_js():
+    filename = './node_modules/@angular/compiler-cli/src/ngtsc/incremental/src/state.js'
+    text_to_search = 'if (this.modifiedResourceFiles === undefined || !this.metadata.has(sf))'
+    replacement_text = 'if (this.modifiedResourceFiles === undefined || this.modifiedResourceFiles === null || !this.metadata.has(sf))'
+
+    with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
+      for line in file:
+        print(line.replace(text_to_search, replacement_text), end='')
+
+    print ('Done!')
+
+AngularBug().fix_state_js()
