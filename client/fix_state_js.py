4ce442981fc0422193aec785226591d4f426d2e5
bag33188 <shuckle168@gmail.com>
2019-08-05 12:00:30 -0700

    fdsa

--- client/fix_state_js.py
+++ client/fix_state_js.py
@@ -18,7 +18,7 @@ def fix_state_js():
   filepath = './node_modules/@angular/compiler-cli/src/ngtsc/incremental/src/state.js'
   text_to_search = 'if (this.modifiedResourceFiles === undefined || !this.metadata.has(sf))'
   replacement_text = 'if (this.modifiedResourceFiles === undefined || this.modifiedResourceFiles === null || !this.metadata.has(sf))'
-  fixApplied = False
+  fix_applied = False
 
   # open up file stream (read and write) and create backup of file
   with fileinput.FileInput(filepath, inplace=True, backup='.bak') as file:
@@ -29,13 +29,13 @@ def fix_state_js():
         # replace text and end line without newline
         print(line.replace(text_to_search, replacement_text), end='')
         # set to true
-        fixApplied = True
+        fix_applied = True
       # check if fix has already been applied
       elif (replacement_text in line):
         # undo fix
         print(line.replace(replacement_text, text_to_search), end='')
         # set to false
-        fixApplied = False
+        fix_applied = False
       else:
         # print other lines
         print(line, end='')
@@ -44,7 +44,7 @@ def fix_state_js():
   fileinput.close()
 
   # if fix has been applied
-  if fixApplied:
+  if fix_applied:
     # tell the user that the fix has been applied
     print('Fix applied!')
   else:
