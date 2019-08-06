e0f4860865755024c7ff9380b59d137ce9f7cc32
bag33188 <shuckle168@gmail.com>
2019-08-06 12:02:32 -0700

    fds

--- client/src/app/interceptors/jwt.interceptor.ts
+++ client/src/app/interceptors/jwt.interceptor.ts
@@ -10,10 +10,16 @@ import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { CookiesService } from '../services/cookies.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private jwtHelper: JwtHelperService,
+    private cookieService: CookiesService
+  ) {}
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
@@ -22,7 +28,16 @@ export class JwtInterceptor implements HttpInterceptor {
       tap(
         (event: HttpEvent<any>): void => {
           if (event instanceof HttpResponse) {
-            // do stuff with response
+            if (
+              this.jwtHelper.isTokenExpired(
+                this.cookieService.getCookie('token_id')
+              ) ||
+              this.cookieService.getCookie('token_id') === ''
+            ) {
+              localStorage.clear();
+              this.cookieService.setCookie('token_id', '', 0);
+              this.router.navigate(['/', 'login']);
+            }
           }
         },
         (err: any): void => {
