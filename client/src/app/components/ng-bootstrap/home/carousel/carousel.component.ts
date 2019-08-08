c0a7e35ab6b9618cce2de4a25acbfe0f95e16ac3
bag33188 <bglatman@callutheran.edu>
2019-08-08 14:07:11 -0700

    dat

--- client/src/app/components/ng-bootstrap/home/carousel/carousel.component.ts
+++ client/src/app/components/ng-bootstrap/home/carousel/carousel.component.ts
@@ -18,25 +18,25 @@ export class CarouselComponent implements OnInit {
     this.images = Images;
     this.carouselData = [
       {
-        image: this.images.IMG1,
+        image: this.images.C_IMG1,
         heading: he.decode('Now Introducing a New Pok&eacute;mon ROMs Website'),
         caption: he.decode('For All Core Pok&eacute;mon Games')
       },
       {
-        image: this.images.IMG2,
+        image: this.images.C_IMG2,
         heading: he.decode(`Including Pok&eacute;mon Let's Go Games`),
         caption: he.decode(`This database includes ROMs for Pok&eacute;mon Let's Go Pikachu &amp;
         Eevee`)
       },
       {
-        image: this.images.IMG3,
+        image: this.images.C_IMG3,
         heading: he.decode('Now with Pok&eacute;mon ROM Hacks'),
         caption: he.decode(
           'You can now play your favorite Pok&eacute;mon ROM hacks'
         )
       },
       {
-        image: this.images.IMG4,
+        image: this.images.C_IMG4,
         heading: he.decode('Pok&eacute;mon Sword and Shield Coming Soon!'),
         caption: he.decode(
           'The new Pok&eacute;mon games will be available on this site 1 week after their release in the US.'
--- client/src/app/enums/images.enum.ts
+++ client/src/app/enums/images.enum.ts
@@ -1,9 +1,8 @@
 export enum Images {
-  IMG0 = '/assets/images/carousel_img_00.png',
-  IMG1 = '/assets/images/carousel_img_01.png',
-  IMG2 = '/assets/images/carousel_img_02.png',
-  IMG3 = '/assets/images/carousel_img_03.png',
-  IMG4 = '/assets/images/carousel_img_04.png',
+  C_IMG0 = '/assets/images/carousel_img_00.png',
+  C_IMG1 = '/assets/images/carousel_img_01.png',
+  C_IMG2 = '/assets/images/carousel_img_02.png',
+  C_IMG3 = '/assets/images/carousel_img_03.png',
+  C_IMG4 = '/assets/images/carousel_img_04.png',
   SPINNER = '/assets/images/spinner.gif'
 }
-
