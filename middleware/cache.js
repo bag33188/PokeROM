fb6b1c400180cd5141fbc914f703309e69dd2d2c
bag33188 <bglatman@callutheran.edu>
2019-08-09 11:14:00 -0700

    fix

--- middleware/cache.js
+++ middleware/cache.js
@@ -3,9 +3,6 @@ const mcache = require('memory-cache');
 function cache(duration) {
   return (req, res, next) => {
     let key = `__express__${req.originalUrl}`;
-    if (req.params['id']) {
-      key = key.replace(`/${req.params['id']}`, '');
-    }
     const queryParams = ['_limit', 'core', 'hacks', 'page', 'per_page'];
     if (Object.keys(req.query).length > 0) {
       queryParams.forEach(param => {
@@ -36,17 +33,10 @@ function cache(duration) {
 
 function clearCache(req) {
   let key = `__express__${req.originalUrl}`;
-  const routeParams = [
-    'all',
-    'core',
-    'hacks',
-    'id',
-    'register',
-    'authenticate'
-  ];
+  const routeParams = ['all', 'core', 'hacks', 'register', 'authenticate'];
   routeParams.forEach(param => {
-    if (req.params[param] || req.originalUrl.includes(param)) {
-      key = key.replace(`/${req.params[param] || param}`, '');
+    if (req.originalUrl.includes(param)) {
+      key = key.replace(`/${param}`, '');
     }
   });
   const queryParams = ['hacks', 'core'];
