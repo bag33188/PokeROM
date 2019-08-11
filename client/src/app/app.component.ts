688454144bc5fa234baa6b7b53c1cb45a312ff86
bag33188 <bglatman@callutheran.edu>
2019-08-11 05:43:58 -0700

    add client src code

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LoggerService as logger } from './services/logger.service';
+import { ApiService } from './services/api.service';
+import { ApiVersion } from './models/ApiVersion';
+import { environment } from '../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <ng-container>
+      <!-- begin page container/wrapper -->
+      <div attr.data-name="{{ title }}" id="container-wrapper">
+        <section>
+          <app-header></app-header>
+        </section>
+        <section>
+          <app-body></app-body>
+        </section>
+        <section [ngClass]="{ 'bg-info': isHomePage() }">
+          <app-footer [appName]="title"></app-footer>
+        </section>
+      </div>
+      <!-- end page container/wrapper -->
+    </ng-container>
+  `,
+  styles: [
+    `
+      /* CSS Global Vars */
+      ::ng-deep :root {
+        --mp-prop-val: auto;
+      }
+    `,
+    `
+      #container-wrapper {
+        display: initial;
+        margin: var(--mp-prop-val);
+        padding: var(--mp-prop-val);
+      }
+    `
+  ]
+})
+export class AppComponent {
+  readonly title: string = 'PokéROM';
+
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute
+  ) {
+    this.changeTitleIfDevEnv();
+    this.getApiVersionIfDevEnv();
+    if (location.protocol === 'https:') {
+      this.apiService.storeApiVersionInCache();
+    }
+  }
+
+  changeTitleIfDevEnv(): void {
+    if (!environment.production) {
+      document.title = `${this.title} (Beta)`;
+    }
+  }
+
+  getApiVersionIfDevEnv(): void {
+    if (!environment.production) {
+      this.apiService.getApiVersion().subscribe(
+        (res: ApiVersion): void => {
+          logger.log(`API Version: ${res.api_version}`);
+        },
+        (err: any): never => {
+          throw err;
+        }
+      );
+    }
+  }
+
+  isHomePage(): boolean {
+    const routeKey: string = '_routerState';
+    return this.route[routeKey].snapshot.url === '/home';
+  }
+}
---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import { Environment } from '../app/interfaces/Environment';
+
+export const environment: Environment = {
+  production: true,
+  apiUrl: 'https://pokerom-broccolini.herokuapp.com/api'
+};
