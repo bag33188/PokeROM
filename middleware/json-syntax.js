38d8ca7e8692b2684ceed12271aae07272f7cc22
bag33188 <bglatman@callutheran.edu>
2019-08-11 05:41:19 -0700

    add 400 err code catcher

---
+++ middleware/json-syntax.js
@@ -0,0 +1,9 @@
+function jsonSyntax(err, req, res, next) {
+  if (err instanceof SyntaxError && err.status === 400) {
+    res.status(400).json({ success: false, message: 'Invalid JSON.' });
+  } else {
+    next();
+  }
+}
+
+module.exports = jsonSyntax;
