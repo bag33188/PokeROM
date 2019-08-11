8a31f4fcb5064689ad81c02c3443120e9c70f8fa
bag33188 <bglatman@callutheran.edu>
2019-08-11 05:42:01 -0700

    add mongoose models

---
+++ models/Nature.js
@@ -0,0 +1,154 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// create mongoose schema
+const natureSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'The name of the nature is required.'],
+      minlength: [3, 'Nature must be at least 3 characters.'],
+      maxlength: [10, 'Nature can only be 10 characters at max.']
+    },
+    up: {
+      type: String,
+      required: [true, 'The increased stat of the nature is required.'],
+      minlength: [
+        4,
+        'The increased stat of the nature  must be at least 4 characters.'
+      ],
+      maxlength: [
+        20,
+        'The increased stat of the nature  can only be 20 characters at max.'
+      ]
+    },
+    down: {
+      type: String,
+      required: [true, 'The decreased stat of the nature is required.'],
+      minlength: [
+        4,
+        'The decreased stat of the nature  must be at least 4 characters.'
+      ],
+      maxlength: [
+        20,
+        'The decreased stat of the nature can only be 20 characters at max.'
+      ]
+    },
+    flavor: {
+      type: String,
+      required: false,
+      minlength: [4, 'The flavor of the nature must be at least 4 characters.'],
+      maxlength: [
+        14,
+        'The flavor of the nature can only be 14 characters at max.'
+      ]
+    },
+    usage: {
+      type: String,
+      required: [true, 'The usage for the nature is required'],
+      minlength: [5, 'The usage for the nature must be at least 5 characters.'],
+      maxlength: [
+        40,
+        'The usage for the nature can only be 40 characters at max.'
+      ]
+    }
+  },
+  {
+    versionKey: false
+  }
+);
+
+// create ROM model
+const Nature = (module.exports = mongoose.model('Nature', natureSchema));
+
+/**
+ * @summary Get All Natures
+ * @description Gets all natures from the database.
+ * @param {Response} callback The callback function.
+ */
+module.exports.getNatures = callback => {
+  Nature.find(callback).sort({
+    up: 1,
+    down: 1
+  });
+};
+
+/**
+ * @summary Get Single Nature.
+ * @description Gets a single nature from the database.
+ * @param {object} query The query to grab the nature with (usually the ID query).
+ * @param {Response} callback The callback function.
+ */
+module.exports.getNature = (query, callback) => {
+  Nature.findById(query, callback);
+};
+
+/**
+ * @summary Patch Nature.
+ * @description Partially updates a nature in the database.
+ * @param {object} idQuery The ID query that finds the nature to partially update.
+ * @param {object} query The data to update the nature with.
+ * @param {Response} callback The callback function.
+ */
+module.exports.patchNature = (idQuery, query, callback) => {
+  Nature.updateOne(idQuery, query, callback);
+};
+
+/**
+ * @summary Delete Nature
+ * @description Deletes one nature in the database.
+ * @param {object} query The id query for locating which nature to delete.
+ * @param {Response} callback The callback function
+ */
+module.exports.deleteNature = (query, callback) => {
+  Nature.deleteOne(query, callback);
+};
+
+/**
+ * @summary Delete All Natures
+ * @description Deletes all natures in the database.
+ * @param {any} callback The callback function.
+ */
+module.exports.deleteAllNatures = callback => {
+  Nature.deleteMany(callback);
+};
+
+/**
+ * @summary Add Nature
+ * @description Adds a nature to the database.
+ * @param {Nature} newNature The new nature to add.
+ * @param {Response} callback The callback function.
+ */
+module.exports.addNature = (newNature, callback) => {
+  Nature.create(newNature, callback);
+};
+
+/**
+ * @summary Update Nature
+ * @description Updates a nature in the database.
+ * @param {object} query The query to find the nature.
+ * @param {Nature} natureData The data to update the nature with.
+ * @param {object} options Any options (none in this case).
+ * @param {any} callback The callback function.
+ */
+module.exports.updateNature = (query, natureData, options, callback) => {
+  const { name, up, down, flavor, usage } = natureData;
+  const natureQuery = {
+    name,
+    up,
+    down,
+    flavor,
+    usage
+  };
+  Nature.findOneAndUpdate(query, natureQuery, options, callback);
+};
+
+/**
+ * @summary Post All Natures
+ * @description Adds all natures to the database.
+ * @param {Array<Nature>} allNatures The natures array containing all natures.
+ */
+module.exports.postAll = (allNatures, callback) => {
+  Nature.insertMany(allNatures, callback);
+};
---
+++ models/Rating.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+function setDate(offset) {
+  let now = new Date();
+  now.setHours(now.getHours() - offset);
+  return now;
+}
+
+const Schema = mongoose.Schema;
+
+const ratingSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      required: [true, 'A rating number is required.'],
+      min: [1, 'Rating must be at least 1.'],
+      max: [10, 'Rating can only be 10 at max.']
+    },
+    message: {
+      type: String,
+      required: false,
+      maxlength: [1000, 'Rating message can only be 1000 characters at max.']
+    },
+    date_time: {
+      type: Date,
+      required: [
+        true,
+        'A date and time is required for when the rating was made.'
+      ],
+      default: setDate(7)
+    }
+  },
+  {
+    versionKey: false
+  }
+);
+
+const Rating = (module.exports = mongoose.model('Rating', ratingSchema));
+
+module.exports.getRatings = (callback, limit) => {
+  Rating.find(callback)
+    .sort({
+      rating: 1,
+      date_time: -1
+    })
+    .limit(parseInt(limit, 10));
+};
+
+module.exports.getRating = (query, callback) => {
+  Rating.findById(query, callback);
+};
+
+module.exports.deleteRating = (query, callback) => {
+  Rating.deleteOne(query, callback);
+};
+
+module.exports.deleteAllRatings = callback => {
+  Rating.deleteMany(callback);
+};
+
+module.exports.addRating = (newRating, callback) => {
+  Rating.create(newRating, callback);
+};
