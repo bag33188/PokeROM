c6e32623bff1dd911b7cbe493d50beba889b93e1
bag33188 <bglatman@callutheran.edu>
2019-08-14 13:42:53 -0700

    add animations index

---
+++ client/src/app/animations/index.ts
@@ -0,0 +1,21 @@
+import {
+  fadeInAnimation,
+  flipAnimation,
+  fadeOutAnimation,
+  flyFromBottomAnimation,
+  flyInAnimation,
+  flyItemsAnimation,
+  rotateInAnimation,
+  slideInAnimation
+} from './animations';
+
+export {
+  fadeOutAnimation,
+  fadeInAnimation,
+  flyItemsAnimation,
+  flyInAnimation,
+  flyFromBottomAnimation,
+  flipAnimation,
+  rotateInAnimation,
+  slideInAnimation
+};
