14c9224d364fc1dac04c0dbec7e99d9b130560c7
bag33188 <bglatman@callutheran.edu>
2019-08-14 16:30:40 -0700

    add comments

--- scripts/count_files_git.rb
+++ scripts/count_files_git.rb
@@ -1,7 +1,19 @@
 #!/usr/bin/env ruby
 
+=begin
+Count Files Git Ruby Script
+---------------------------
+To run:
+  ruby count_files_git.rb
+Notes1:
+  * Must have ruby installed
+=end
+
+# define main function
 def count_files_git()
+  # execute commands to count files on github
 	system('cd .. && git ls-files | wc -l')
 end
 
+# call main function
 count_files_git()
