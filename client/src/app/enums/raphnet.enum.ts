b63b994036af4b3a3604c0e496d295b10187e1a2
bag33188 <bglatman@callutheran.edu>
2019-08-23 07:37:47 -0700

    update acc

--- client/src/app/enums/raphnet.enum.ts
+++ client/src/app/enums/raphnet.enum.ts
@@ -1,4 +1,5 @@
 export enum Raphnet {
+  WEBSITE = 'https://www.raphnet-tech.com',
   SOFTWARE = 'https://www.raphnet-tech.com/products/adapter_manager/index.php',
   PRODUCT = 'https://www.raphnet-tech.com/products/n64_usb_adapter_gen3/index.php'
 }
