78605bf5b9dde27ba1e5c6a316af7cf9bb1473e6
bag33188 <bglatman@callutheran.edu>
2019-08-27 08:36:31 -0700

    remove comments

--- docs/swagger-doc.js
+++ docs/swagger-doc.js
@@ -2,9 +2,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerDefinition = require('./swagger-definition.json');
 
-// Swagger Definition: swagger doc info object
-// Like the one described here: https://swagger.io/specification/#infoObject
-// './docs/**/*.yml'
+// https://swagger.io/specification/#infoObject
 
 // get api docs version
 const version = swaggerDefinition.swaggerDefinition.info.version;
@@ -23,11 +21,6 @@ parseVersion();
 // define swagger entity
 const specs = swaggerJsDoc(swaggerDefinition);
 
-/**
- * @summary API Docs middleware.
- * @description Configures middleware for Swagger Docs.
- * @param {object} app expressJS app object.
- */
 const apiDocs = app =>
   app.use(`/api/docs/${version}`, swaggerUi.serve, swaggerUi.setup(specs));
 
