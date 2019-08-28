03ea103ab32bbc1a59c3eea4150e2034c567c323
bag33188 <bglatman@callutheran.edu>
2019-08-28 12:19:02 -0700

    update license data

--- scripts/add_licenses.rb
+++ scripts/add_licenses.rb
@@ -31,9 +31,14 @@ class AddLicenses
     "\n" +
     "Copyright #{Date.today.year} #{name}" +
     "\n" +
-    'Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.' +
+    'Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby ' +
+    'granted, provided that the above copyright notice and this permission notice appear in all copies.' +
     "\n" +
-    'THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.'
+    'THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ' +
+    'ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, ' +
+    'DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR ' +
+    'PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION ' +
+    'WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.'
 
     # return the license text
     return isc_license
@@ -47,11 +52,19 @@ class AddLicenses
     "\n" +
     "Copyright #{Date.today.year} #{name}" +
     "\n" +
-    'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:' +
+    'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated ' +
+    'documentation files (the "Software"), to deal in the Software without restriction, including without limitation ' +
+    'the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and' +
+    ' to permit persons to whom the Software is furnished to do so, subject to the following conditions:' +
     "\n" +
-    'The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.' +
+    'The above copyright notice and this permission notice shall be included in all copies or substantial portions ' +
+    'of the Software.' +
     "\n" +
-    'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'
+    'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED ' +
+    'TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. ' +
+    'IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, ' +
+    'WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR ' +
+    'THE USE OR OTHER DEALINGS IN THE SOFTWARE.'
 
     # return license text
     return mit_license
@@ -62,8 +75,21 @@ class AddLicenses
     # create new file or open existing one from filepath parameter
     file = File.open(filepath, 'w')
 
+    # construct license data variable
+    license_data = '' +
+    'pokerom' +
+    "\n" +
+    'ISC' +
+    "\n" +
+    'MIT' +
+    "\n\n" +
+    "#{get_isc_license(@name)}" +
+    "\n\n" +
+    "#{get_mit_license(@name)}" +
+    "\n"
+
     # write licenses to file
-    file.write("pokerom\nISC\nMIT\n\n#{get_isc_license(@name)}\n\n#{get_mit_license(@name)}\n")
+    file.write(license_data)
 
     # close the file stream
     file.close
