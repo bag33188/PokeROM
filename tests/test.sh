e16705a5f4e850f9d7c2cfc070c9adeb058b6062
bag33188 <bglatman@callutheran.edu>
2019-09-04 08:13:54 -0700

    update test script

--- tests/test.sh
+++ tests/test.sh
@@ -19,30 +19,28 @@ $ chmod 755 ./test.sh
 
 test() {
   echo -e "Endpoints\n---------\n\n1. Natures\n2. Options\n3. Ratings\n4. Roms\n5. Users\n6. Version\n7. Exit\n\n"
-  read -r -p 'Which endpoint collection would you like to test? ' endpoint
-  if [[ $endpoint == 1 ]]; then
-    bash ./natures.sh
-    cd ..
-  elif [[ $endpoint == 2 ]]; then
-    bash ./options.sh
-    cd ..
-  elif [[ $endpoint == 3 ]]; then
-    bash ./ratings.sh
-    cd ..
-  elif [[ $endpoint == 4 ]]; then
-    bash ./roms.sh
-    cd ..
-  elif [[ $endpoint == 5 ]]; then
-    bash ./users.sh
-    cd ..
-  elif [[ $endpoint == 6 ]]; then
-    bash ./version.sh
-    cd ..
-  elif [[ $endpoint == 7 ]]; then
-    cd ..
-  else
-    echo 'Invalid option.'
-  fi
+  while true; do
+    read -r -p 'Which endpoint collection would you like to test? ' endpoint
+    if [[ $endpoint == 1 ]]; then
+      bash ./natures.sh
+    elif [[ $endpoint == 2 ]]; then
+      bash ./options.sh
+    elif [[ $endpoint == 3 ]]; then
+      bash ./ratings.sh
+    elif [[ $endpoint == 4 ]]; then
+      bash ./roms.sh
+    elif [[ $endpoint == 5 ]]; then
+      bash ./users.sh
+    elif [[ $endpoint == 6 ]]; then
+      bash ./version.sh
+    elif [[ $endpoint == 7 ]]; then
+      break
+    else
+      echo 'Invalid option.'
+      continue
+    fi
+  done
+  cd ..
   printf "\n"
 }
 
