c109f1dd9d2659c9c590b9d3cb73570aff7f5479
bag33188 <bglatman@callutheran.edu>
2019-09-06 18:43:15 -0700

    f

--- scripts/git_permissions.sh
+++ scripts/git_permissions.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# ================
-#  Git Permissions
-# ================
+# =======================
+#  Git Permissions Script
+# =======================
 
 # shellcheck disable=SC2188
 << --MULTILINE-COMMENT--
