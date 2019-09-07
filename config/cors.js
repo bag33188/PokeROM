08c65d451a7687079476aa05aa18ffe45fd74062
bag33188 <bglatman@callutheran.edu>
2019-09-07 11:45:43 -0700

    fix

--- config/cors.js
+++ config/cors.js
@@ -1,49 +1,50 @@
 const cors = require('cors');
 
-module.exports = cors({
-  credentials: true,
-  exposedHeaders: [
-    'Allow',
-    'Cache-Control',
-    'Content-Language',
-    'Content-Type',
-    'Expires',
-    'Last-Modified',
-    'Pragma',
-    'Content-Range',
-    'X-Content-Range',
-    'X-Requested-With'
-  ],
-  headers: [
-    'Accept',
-    'Accept-Language',
-    'Allow',
-    'Authorization',
-    'Content-Type',
-    'Content-Language',
-    'Save-Data',
-    'Viewport-Width',
-    'Width',
-    'X-Requested-With'
-  ],
-  maxAge: 86400,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-  optionsSuccessStatus: 204,
-  origin:
-    process.env.NODE_ENV === 'production'
-      ? function x(req, res, next) {
-          if (
-            /http(s)?:\/\/(www\.)?pokerom\.dev(\/)?/.test(
-              `${req.protocol}://${req.get('host')}${req.originalUrl}`
-            )
-          ) {
-            console.log(
-              `${req.protocol}://${req.get('host')}${req.originalUrl}`
-            );
-            return `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-          } else {
-            return false;
+module.exports = (req, res, next) => {
+  return cors({
+    credentials: true,
+    exposedHeaders: [
+      'Allow',
+      'Cache-Control',
+      'Content-Language',
+      'Content-Type',
+      'Expires',
+      'Last-Modified',
+      'Pragma',
+      'Content-Range',
+      'X-Content-Range',
+      'X-Requested-With'
+    ],
+    headers: [
+      'Accept',
+      'Accept-Language',
+      'Allow',
+      'Authorization',
+      'Content-Type',
+      'Content-Language',
+      'Save-Data',
+      'Viewport-Width',
+      'Width',
+      'X-Requested-With'
+    ],
+    maxAge: 86400,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    optionsSuccessStatus: 204,
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? (origin, callback) => {
+            if (
+              ['https://pokerom.dev', 'https://www.pokerom.dev'].indexOf(
+                origin
+              ) !== -1
+            ) {
+              callback(null, true);
+            } else if (!origin) {
+              callback(null, 'https://pokerom.dev');
+            } else {
+              callback(new Error('Not allowed by CORS'));
+            }
           }
-        }
-      : 'http://localhost:4200'
-});
+        : 'http://localhost:4200'
+  });
+};
