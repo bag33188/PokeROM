5c08dbed9ec4e372112c806fa3483a1d46d8a20d
bag33188 <bglatman@callutheran.edu>
2019-09-07 11:50:54 -0700

    fix cors issue

--- config/cors.js
+++ config/cors.js
@@ -1,50 +1,48 @@
 const cors = require('cors');
 
-module.exports = (req, res, next) => {
-  return cors({
-    credentials: true,
-    exposedHeaders: [
-      'Allow',
-      'Cache-Control',
-      'Content-Language',
-      'Content-Type',
-      'Expires',
-      'Last-Modified',
-      'Pragma',
-      'Content-Range',
-      'X-Content-Range',
-      'X-Requested-With'
-    ],
-    headers: [
-      'Accept',
-      'Accept-Language',
-      'Allow',
-      'Authorization',
-      'Content-Type',
-      'Content-Language',
-      'Save-Data',
-      'Viewport-Width',
-      'Width',
-      'X-Requested-With'
-    ],
-    maxAge: 86400,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-    optionsSuccessStatus: 204,
-    origin:
-      process.env.NODE_ENV === 'production'
-        ? (origin, callback) => {
-            if (
-              ['https://pokerom.dev', 'https://www.pokerom.dev'].indexOf(
-                origin
-              ) !== -1
-            ) {
-              callback(null, true);
-            } else if (!origin) {
-              callback(null, 'https://pokerom.dev');
-            } else {
-              callback(new Error('Not allowed by CORS'));
-            }
+module.exports = cors({
+  credentials: true,
+  exposedHeaders: [
+    'Allow',
+    'Cache-Control',
+    'Content-Language',
+    'Content-Type',
+    'Expires',
+    'Last-Modified',
+    'Pragma',
+    'Content-Range',
+    'X-Content-Range',
+    'X-Requested-With'
+  ],
+  headers: [
+    'Accept',
+    'Accept-Language',
+    'Allow',
+    'Authorization',
+    'Content-Type',
+    'Content-Language',
+    'Save-Data',
+    'Viewport-Width',
+    'Width',
+    'X-Requested-With'
+  ],
+  maxAge: 86400,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  optionsSuccessStatus: 204,
+  origin:
+    process.env.NODE_ENV === 'production'
+      ? (origin, callback) => {
+          if (
+            ['https://pokerom.dev', 'https://www.pokerom.dev'].indexOf(
+              origin
+            ) !== -1
+          ) {
+            callback(null, true);
+          } else if (!origin) {
+            callback(null, 'https://pokerom.dev');
+          } else {
+            callback(new Error('Not allowed by CORS'));
           }
-        : 'http://localhost:4200'
-  });
-};
+        }
+      : 'http://localhost:4200'
+});
