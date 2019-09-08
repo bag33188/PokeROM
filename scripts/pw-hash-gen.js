0a6d49c187387382cada7f14941fb67017110646
bag33188 <bglatman@callutheran.edu>
2019-09-08 07:54:35 -0700

    change to class structure

--- scripts/pw-hash-gen.js
+++ scripts/pw-hash-gen.js
@@ -1,24 +1,43 @@
-const bcrypt = require('bcryptjs');
 const readline = require('readline');
+const bcrypt = require('bcryptjs');
 
-// create readline interface
-const rlInterface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// ask user for password
-rlInterface.question('Enter password: ', pw => {
-  // generate salt using bcrypt
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) console.log(err);
-    // generate password hash using generated salt
-    bcrypt.hash(pw, salt, (err, hash) => {
-      if (err) console.log(err);
-      // output hashed password
-      console.log(`Hashed password: ${hash}`);
+class PwHashGen {
+  constructor() {
+    // create readline interface
+    this.rlInterface = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
     });
-  });
-  // close readline interface
-  rlInterface.close();
-});
+  }
+
+  /**
+   * @summary Password Hash Generator.
+   * @description Generates a hash from input password.
+   * @return {void} Nothing.
+   */
+  genPw() {
+    // ask user for password
+    this.rlInterface.question('Enter password: ', pw => {
+      // generate salt using bcrypt
+      bcrypt.genSalt(10, (err, salt) => {
+        // check for errors
+        if (err) console.log(err);
+        // generate password hash using generated salt
+        bcrypt.hash(pw, salt, (err, hash) => {
+          // check for errors
+          if (err) console.log(err);
+          // output hashed password
+          console.log(`Hashed password: ${hash}`);
+        });
+      });
+      // close readline interface
+      this.rlInterface.close();
+    });
+  }
+}
+
+// instantiate `PwHashGen` class.
+const PasswordHashGenerator = new PwHashGen();
+
+// invoke `genPw` method from `PwHashGen` class
+PasswordHashGenerator.genPw();
