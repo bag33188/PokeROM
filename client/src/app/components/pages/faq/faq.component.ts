5b2e30292fffd5d72d74d16991fb8c2e6fea8be3
Broccolini <bglatman@callutheran.edu>
2019-09-09 08:54:23 -0700

    update code base

--- client/src/app/components/pages/faq/faq.component.ts
+++ client/src/app/components/pages/faq/faq.component.ts
@@ -17,11 +17,16 @@ export class FaqComponent implements OnInit, AfterContentInit {
   @ViewChild('cookies', { static: true }) private cookiesElement: ElementRef;
   @ViewChild('browserCompatibility', { static: true })
   private browserCompatibilityElement: ElementRef;
+  public computerUrls: string[];
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.emulatorUrl = 'https://www.retroarch.com';
+    this.computerUrls = [
+      'https://www.nvidia.com/en-us/geforce/graphics-cards/rtx-2080-ti/', // cpu
+      'https://www.intel.com/content/www/us/en/products/processors/core/i9-processors/i9-9900k.html' // gpu
+    ];
   }
 
   ngAfterContentInit(): void {
