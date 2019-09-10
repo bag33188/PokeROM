648298c56c38228ebdfb04214857a2e34b267233
bag33188 <bglatman@callutheran.edu>
2019-09-10 14:45:36 -0700

    fix path

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,5 +3,5 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: './src',
-  modules: ['node_modules', path.resolve(__dirname, 'src/app')]
+  modules: ['node_modules', path.resolve(__dirname, path.join('src', 'app'))]
 };
