32b3079e567f3478e35849ae438b247e166c1875
bag33188 <bglatman@callutheran.edu>
2019-09-19 11:38:23 -0700

    update static files

--- client/src/app/enums/images.enum.ts
+++ client/src/app/enums/images.enum.ts
@@ -1,11 +1,8 @@
 export enum Images {
-  C_IMG0 = '/assets/images/carousel_img_00.png',
   C_IMG1 = '/assets/images/carousel_img_01.jpg',
   C_IMG2 = '/assets/images/carousel_img_02.jpg',
   C_IMG3 = '/assets/images/carousel_img_03.jpg',
   C_IMG4 = '/assets/images/carousel_img_04.jpg',
   SPINNER = '/assets/images/spinner.gif',
-  LOGO_1 = '/assets/images/logo-1.png',
-  LOGO_2 = '/assets/images/logo-2.png',
-  LOGO_ORIGINAL = '/assets/images/logo-original.png'
+  LOGO = '/assets/images/logo-original.png'
 }
