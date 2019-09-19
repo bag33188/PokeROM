00028ebe965b392b6137ebf287ee7c0e0b3cbbaa
bag33188 <bglatman@callutheran.edu>
2019-09-19 11:31:44 -0700

    compress images

--- client/src/app/enums/images.enum.ts
+++ client/src/app/enums/images.enum.ts
@@ -1,9 +1,9 @@
 export enum Images {
   C_IMG0 = '/assets/images/carousel_img_00.png',
-  C_IMG1 = '/assets/images/carousel_img_01.png',
-  C_IMG2 = '/assets/images/carousel_img_02.png',
-  C_IMG3 = '/assets/images/carousel_img_03.png',
-  C_IMG4 = '/assets/images/carousel_img_04.png',
+  C_IMG1 = '/assets/images/carousel_img_01.jpg',
+  C_IMG2 = '/assets/images/carousel_img_02.jpg',
+  C_IMG3 = '/assets/images/carousel_img_03.jpg',
+  C_IMG4 = '/assets/images/carousel_img_04.jpg',
   SPINNER = '/assets/images/spinner.gif',
   LOGO_1 = '/assets/images/logo-1.png',
   LOGO_2 = '/assets/images/logo-2.png',
