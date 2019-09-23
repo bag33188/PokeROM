d800ed0c7ca05283eb0d07f41a3bc2eb844c33b5
bag33188 <bglatman@callutheran.edu>
2019-09-23 06:21:43 -0700

    fix

--- client/src/app/components/roms/rom-info/game-description/game-description.component.ts
+++ client/src/app/components/roms/rom-info/game-description/game-description.component.ts
@@ -10,7 +10,7 @@ export class GameDescriptionComponent implements OnInit {
 
   constructor() {}
   static changeUrlToLink(description: string): string {
-    const urlRegex: RegExp = /(?:(http(s)?):\/\/(www\.)?[a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))/i;
+    const urlRegex: RegExp = /(?:(http(s)?):\/\/(www\.)?[a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))/gim;
     if (urlRegex.test(description)) {
       const url: string = description.match(urlRegex)[0];
       return description.replace(
