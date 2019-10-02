31a4f7b76ef042bb4d2d4ec87cee607528849bef
bag33188 <bglatman@callutheran.edu>
2019-10-02 08:59:03 -0700

    update code base

--- www/languages.php
+++ www/languages.php
@@ -56,7 +56,7 @@
       } else if (document.addEventListener) {
         document.addEventListener("DOMContentLoaded", callback);
       } else {
-        document.attachEvent("onreadystatechange", function () {
+        document.attachEvent("onreadystatechange", () => {
           if (document.readyState === "complete") callback();
         });
       }
