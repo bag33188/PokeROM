071b1980a6315c1b23d4df1dec0d1c3079687c5a
bag33188 <bglatman@callutheran.edu>
2019-10-02 12:38:47 -0700

    fix cache

--- middleware/cache.js
+++ middleware/cache.js
@@ -1,53 +1,12 @@
 const mcache = require('memory-cache');
 const cacheControl = require('express-cache-controller');
 
-function removeURIComponents(req, method, key) {
-  let queryParams;
-  let routeParams;
-  switch (method.toUpperCase()) {
-    case 'GET':
-      queryParams = ['_limit', 'core', 'hacks', 'page', 'per_page'];
-      routeParams = [];
-      break;
-    case 'POST':
-    case 'PUT':
-    case 'PATCH':
-    case 'DELETE':
-      routeParams = ['all', 'core', 'hacks', 'register'];
-      queryParams = ['hacks', 'core'];
-      break;
-    default:
-      routeParams = [];
-      queryParams = [];
-      break;
-  }
-  if (queryParams.length > 0) {
-    if (Object.keys(req.query).length > 0) {
-      queryParams.forEach(param => {
-        key = key
-          .replace(req.query[param], '')
-          .replace(param, '')
-          .replace('?', '')
-          .replace(/([&=])/g, '');
-      });
-    }
-  }
-  if (routeParams.length > 0) {
-    routeParams.forEach(param => {
-      if (req.originalUrl.includes(`/${param}`)) {
-        key = key.replace(`/${param}`, '');
-      }
-    });
-  }
-  return key;
-}
-
 function cache(duration) {
   return (req, res, next) => {
     let key = `__express__${req.originalUrl}${
       req.user ? ':' + req.user['_id'] : ''
     }`;
-    key = removeURIComponents(req, req.method, key);
+    console.log(1, key);
     const cachedBody = mcache.get(key);
     if (cachedBody) {
       res.send(cachedBody);
@@ -67,7 +26,6 @@ function clearCache(req) {
   let key = `__express__${req.originalUrl}${
     req.user ? ':' + req.user['_id'] : ''
   }`;
-  key = removeURIComponents(req, req.method, key);
   const cachedBody = mcache.get(key);
   if (cachedBody) {
     // remove all cached roms even if single rom is changed
