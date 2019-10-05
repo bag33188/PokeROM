f3d8fce515d389e2c1a92387524d4de2a992a8ab
bag33188 <bglatman@callutheran.edu>
2019-10-05 16:42:11 -0700

    fix

--- www/index.php
+++ www/index.php
@@ -5,18 +5,29 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
     <?php
-      $filePath = "../docs/swagger-definition.yml";
-      $file = fopen($filePath, "r");
-      $fileSize = filesize($filePath);
-      $fileText = fread($file, $fileSize);
-      preg_match("(version: v\d)", $fileText, $version);
-      $apiVersion = str_replace("version: ", "", $version[0]);
+      function getApiVersion() {
+        $filePath = "../docs/swagger-definition.yml";
+        $file = fopen($filePath, "r");
+        $fileSize = filesize($filePath);
+        $fileText = fread($file, $fileSize);
+        preg_match("(version: v\d)", $fileText, $version);
+        $apiVersion = str_replace("version: ", "", $version[0]);
+        return $apiVersion;
+      }
     ?>
-    <meta http-equiv="refresh" content="0;url=/api/docs/<?php echo $apiVersion; ?>" />
+    <meta http-equiv="refresh" content="0;url=/api/docs/<?php echo getApiVersion(); ?>" />
     <title>API Docs (Redirect)</title>
     <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+    <style type="text/css">
+      *, *::before, *::after {
+        box-sizing: border-box !important;
+      }
+      body {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+      }
+    </style>
   </head>
   <body>
-    <h1>Redirecting to <?php echo "/api/docs/$apiVersion"; ?> ... </h1>
+    <h1>Redirecting to <?php echo "<code>/api/docs/" . getApiVersion() . "</code>"; ?> ... </h1>
   </body>
 </html>
