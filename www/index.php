f72f6ff3740450c9b1e9abaf9191523f5c49c548
bag33188 <bglatman@callutheran.edu>
2019-10-05 15:32:25 -0700

    fix

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+    <?php
+      echo "<meta http-equiv=\"refresh\" content=\"0;url=/api/docs/v1\" />";
+    ?>
+    <title>API Docs</title>
+  </head>
+  <body>
+    <?php
+      echo "Loading ... ";
+    ?>
+  </body>
+</html>
