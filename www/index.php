e5b3ca5debfc1e5a98cffa0add9fbe4c83e7b112
bag33188 <bglatman@callutheran.edu>
2019-10-06 11:21:41 -0700

    add comments

--- www/index.php
+++ www/index.php
@@ -9,28 +9,60 @@
        * @return array API Version Data.
        */
       function getApiVersion() {
+        // define file data
         $filePath = "../docs/swagger-definition.yml";
         $file = fopen($filePath, "r");
         $fileSize = filesize($filePath);
         $fileText = fread($file, $fileSize);
-        define("VERSION_REGEX", "/((?:version:)(?:\s)(?:v\d))/i");
+
+        // define regular expression for identifying api version in YAML data
+        define("VERSION_REGEX", "/((?:versaion:)(?:\s)(?:v\d))/i");
+
+        // match api version from file text to version regexp
         $versionExists = preg_match(VERSION_REGEX, $fileText, $version);
+
+        // if api version exists in YAML data ...
         if ($versionExists == true) {
+          // replace version text to extract out api version value
           $apiVersion = str_replace("version: ", "", $version[0]);
+
+          // return php array of object-literal
+          # schema
+          # ======
+          # array(1) {
+          # [0]=>
+          # object(stdClass)#1 (2) {
+          # ["success"]=> bool(true)
+          # ["api_version"]=> string(2) "v\d"
+          # }
+          # }
           return array(
             (object)array("success" => true, "api_version" => $apiVersion)
           );
-        } else {
+        } else { // if it doesn't exist ...
+          // return php array of object-literal
+          # schema
+          # ======
+          # array(1) {
+          # [0]=>
+          # object(stdClass)#1 (2) {
+          # ["success"]=> bool(false)
+          # ["api_version"]=> NULL
+          # }
+          # }
           return array(
             (object)array("success" => false, "api_version" => NULL)
           );
         }
       }
 
+      // set variable to function to make referencing easier
       $apiVersion = getApiVersion();
     ?>
-    <meta http-equiv="refresh" content="0;url=/api/docs/<?php
+    <meta http-equiv="refresh" content="1000000;url=/api/docs/<?php
+      // loop through api version data
       for ($i = 0; $i < count($apiVersion); $i++) {
+        // output the api's version text
         echo $apiVersion[$i]->api_version;
       }
     ?>/" />
@@ -48,9 +80,13 @@
     </style>
     <script type="text/javascript">
       console.log("<?php
+        // loop thru api version data
         for ($i = 0; $i < sizeof($apiVersion); $i++) {
+          // check if api version exists
           echo ($apiVersion[$i]->success == 1) ?
+            // if yes, log the api version to console
             "API Version: " . $apiVersion[$i]->api_version :
+            // if no, log err msg to console
             "Error getting API version.";
         }
       ?>");
@@ -58,9 +94,13 @@
   </head>
   <body>
     <h1>Redirecting to <?php
+      // loop thru api version data
       foreach ($apiVersion as $versionData) {
+        // check if version does not exist
         $apiVersionHTML = ($versionData->success == 0) ?
+          // if version is absent, print out error
           print "<code>ERROR_GETTING_API_VERSION</code>" :
+          // else print out the api's version along the the rest of the api docs url
           print "<code>/api/docs/" . $versionData->api_version . "/</code>";
       }
     ?> ... </h1>
