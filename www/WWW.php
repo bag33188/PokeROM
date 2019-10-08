695d45caf84ab9ebd8e8e54809a2c9765debd45f
bag33188 <bglatman@callutheran.edu>
2019-10-07 17:51:05 -0700

    fix

--- www/WWW.php
+++ www/WWW.php
@@ -1,6 +1,6 @@
 <?php
   http_response_code(404);
-  header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+  # header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
 
   class WWW
   {
