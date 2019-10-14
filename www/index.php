5f9b839286b759e544b9f8ecd50594db217903ae
bag33188 <bglatman@callutheran.edu>
2019-10-14 13:40:18 -0700

    fix

--- www/index.php
+++ www/index.php
@@ -24,6 +24,7 @@
     </style>
     <script type="text/javascript">
       "use strict";
+
       <?php if ($apiVersionData->success == 1): ?>
         console.log("API Version: <?= $apiVersionData->api_version; ?>");
       <?php else: ?>
