20fc0b554f29b196f7b3296cf69571214d467864
bag33188 <bglatman@callutheran.edu>
2019-10-19 16:58:00 -0700

    fix

--- www/head_tags.inc.php
+++ www/head_tags.inc.php
@@ -1,17 +1,21 @@
-<!-- begin meta tags -->
-<?php
-  $head_tags = array(
-    "<meta charset=\"UTF-8\" />",
-    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />",
-    "<meta http-equiv=\"X-UA-Compatible\" content=\"IE-Edge\" />",
-    "<meta name=\"author\" content=\"bag33188, broccolini@pokerom.dev\" />",
-    "<meta name=\"copyright\" content=\"Copyright (c) 2019 bag33188\" />",
-    "<meta name=\"language\" content=\"EN\" />"
-  );
-  for ($i = 0; $i < count($head_tags); $i++) {
-    $head_tag = $head_tags[$i];
-    echo $head_tag . "\n";
-  }
-?>
-<!-- end meta tags -->
-<base href="/" />
+<?php if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) { ?>
+  <?php http_response_code(404); ?>
+<?php } else { ?>
+  <!-- begin meta tags -->
+  <?php
+    $head_tags = array(
+      "<meta charset=\"UTF-8\" />",
+      "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />",
+      "<meta http-equiv=\"X-UA-Compatible\" content=\"IE-Edge\" />",
+      "<meta name=\"author\" content=\"bag33188, broccolini@pokerom.dev\" />",
+      "<meta name=\"copyright\" content=\"Copyright (c) 2019 bag33188\" />",
+      "<meta name=\"language\" content=\"EN\" />"
+    );
+    for ($i = 0; $i < count($head_tags); $i++) {
+      $head_tag = $head_tags[$i];
+      echo $head_tag . "\n";
+    }
+  ?>
+  <!-- end meta tags -->
+  <base href="/" />
+<?php } ?>
