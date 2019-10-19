7596b66a4fb27c956f99e941b76210656cc742bb
bag33188 <bglatman@callutheran.edu>
2019-10-19 15:59:00 -0700

    reorder code

--- www/WWW.php
+++ www/WWW.php
@@ -6,8 +6,8 @@
   use Exception;
 
   // send 404 response code upon request
-  # http_response_code(404);
   header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+  # http_response_code(404);
 
   /**
    * Class WWW
