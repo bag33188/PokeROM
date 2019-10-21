8302ec3d756f8be6bb34a5fc54f8f7bb5ef64191
bag33188 <bglatman@callutheran.edu>
2019-10-20 21:25:04 -0700

    add jsdoc comments

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,16 @@
 const passport = require('passport');
 
+/**
+ * @function
+ * @async
+ * @name auth
+ * @summary Authenticate
+ * @description Authorizes a user.
+ * @param req The request.
+ * @param res The response.
+ * @param next The function to call the next piece of middleware.
+ * @returns {Promise<void>} The authentication promise.
+ */
 async function auth(req, res, next) {
   try {
     return await passport.authenticate(
