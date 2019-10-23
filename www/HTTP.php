aefa733808cb582a6bce1331cee21dce1bbb584f
bag33188 <bglatman@callutheran.edu>
2019-10-23 12:23:56 -0700

    fix

--- www/HTTP.php
+++ www/HTTP.php
@@ -28,8 +28,10 @@
      */
     public function __construct()
     {
+      // create constructor vars
       $currentURL = str_replace($this->getProtocol(), "", $this->currentURL);
       $currentURI = self::getCurrentUrl();
+      // set global class props using private methods
       $this->setCurrentURI($currentURI);
       $this->setCurrentURL($currentURL);
     }
@@ -52,6 +54,7 @@
      * @return string The current HTTP protocol in URL format.
      */
     private function getProtocol() {
+      // check if ssl is being used
       if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] === "on") {
         return "https://";
       } else {
@@ -63,6 +66,7 @@
      * @return mixed The HTTP host.
      */
     private function getHTTPHost() {
+      // store http host in var
       $httpHost = $_SERVER["HTTP_HOST"];
       return $httpHost;
     }
@@ -71,6 +75,7 @@
      * @return mixed The request URI.
      */
     private function getRequestURI() {
+      // store request uri in var
       $requestURI = $_SERVER["REQUEST_URI"];
       return $requestURI;
     }
@@ -79,21 +84,21 @@
      * @return bool If HTTP protocol is being use (or HTTPS).
      */
     public function isHTTP() {
-      if (!strpos($this->getProtocol(), "s")) {
-        return true;
-      } else {
-        return false;
-      }
+      // return true if char `s` is in protocol (https), false if not
+      return (!strpos($this->getProtocol(), "s")) ? true : false;
     }
 
     /**
      * @return string|null The port number being served on localhost.
      */
     public function getPortNumber() {
+      // make sure env is in dev mode
       if (!self::isProductionMode()) {
+        // split http host into array by colon char and grab second item
         $port = explode(":", $this->getHTTPHost())[1];
         return $port;
       } else {
+        // return null if prod env
         return NULL;
       }
     }
@@ -102,10 +107,20 @@
      * @return string|null The page name.
      */
     public function getPageName() {
+      // make sure env is dev
       if (!self::isProductionMode()) {
-        $pageName = preg_replace("/((?:\.?\/)|(?:\.php))/", "", $this->getRequestURI());
+        // make sure const is not already defined
+        if (!defined("EXT_REGEXP")) {
+          // define regexp const
+          define("EXT_REGEXP", "/((?:\.?\/)|(?:\.php))/");
+        }
+        // store page name in var.
+        // detect content in request uri with regexp const and replace with blank string
+        $pageName = preg_replace(EXT_REGEXP, "", $this->getRequestURI());
+        // capitalize the page name
         return ucfirst($pageName);
       } else {
+        // return null if prod env
         return NULL;
       }
     }
@@ -114,10 +129,14 @@
      * @return void Nothing.
      */
     public function redirectToHTTPS() {
+      // make sure protocol is not already https
       if ($this->getProtocol() == "http://") {
+        // set location to protocol plus current url
         $location = $this->getProtocol() . $this->currentURL;
+        // redirect using header
         header("Location: " . $location);
       } else {
+        // exit script with code 0
         exit(0);
       }
     }
