7fe1d08454c6bca3294a46d7c57111999b98e33c
Broccolini <bglatman@callutheran.edu>
2019-11-05 12:19:32 -0800

    fix

--- scripts/print-keys.js
+++ scripts/print-keys.js
@@ -13,25 +13,25 @@ function printKeys() {
   let personalAccessTokensStr = '';
   let deployKeysStr = '';
   const { personalAccessTokens, deployKeys } = doc.keys;
-  personalAccessTokens.forEach((pat, i) => {
-    personalAccessTokensStr += `${(i + 1).toString()}. ${
-      Object.keys(pat)[0]
-    }: ${pat[Object.keys(pat)[0]]}\n`;
-  });
-  deployKeys.forEach((deployKey, i) => {
-    deployKeysStr += `${(i + 1).toString()}. ${
-      Object.keys(deployKey)[0]
-    }:\nPublic:\n${deployKey[Object.keys(deployKey)[0]].public}\nPrivate:\n${
-      deployKey[Object.keys(deployKey)[0]].private
+  personalAccessTokens.forEach((personalAccessToken, i) => {
+    const key = Object.keys(personalAccessToken)[0];
+    personalAccessTokensStr += `${(i + 1).toString()}. ${key}: ${
+      personalAccessToken[key]
     }\n`;
   });
-  const keys = `${parts[0]}\n${'='.repeat(parts[0].length)}\n\n${
+  deployKeys.forEach((deployKey, i) => {
+    const key = Object.keys(deployKey)[0];
+    deployKeysStr += `${(i + 1).toString()}. ${key}:\nPublic:\n${
+      deployKey[key].public
+    }\nPrivate:\n${deployKey[key].private}\n`;
+  });
+  const keysStr = `${parts[0]}\n${'='.repeat(parts[0].length)}\n\n${
     parts[1]
   }\n${'-'.repeat(parts[1].length)}\n${personalAccessTokensStr}\n\n${
     parts[2]
   }\n${'-'.repeat(parts[2].length)}\n${deployKeysStr}\n`;
-  console.log(keys);
-  return keys;
+  console.log(keysStr);
+  return keysStr;
 }
 
 printKeys();
