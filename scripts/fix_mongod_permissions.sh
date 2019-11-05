a16d0f2646bda458608e59b1d5e8e69d4a6b21b6
Broccolini <bglatman@callutheran.edu>
2019-11-05 12:38:07 -0800

    fix

---
+++ scripts/fix_mongod_permissions.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+fix_mongod_permissions() {
+  cd .. || return
+  chown mongod:mongod /tmpmongodb-44380.sock
+  chown -R mongod:mongod /var/lib/mongo
+}
+
+fix_mongod_permissions
