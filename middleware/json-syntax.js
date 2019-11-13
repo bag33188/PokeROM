c937f5058dc69d0bb45ee53c3e15ffe3833af394
Broccolini <bglatman@callutheran.edu>
2019-11-13 12:12:16 -0800

    apply structure

--- middleware/json-syntax.js
+++ middleware/json-syntax.js
@@ -1,6 +1,8 @@
 function jsonSyntax(err, req, res, next) {
-  if (err instanceof SyntaxError && err.status === 400) {
-    res.status(400).json({ success: false, message: 'Invalid JSON.' });
+  if (err instanceof SyntaxError) {
+    if (err.status === 400) {
+      res.status(400).json({ success: false, message: 'Invalid JSON.' });
+    }
   } else {
     next();
   }
