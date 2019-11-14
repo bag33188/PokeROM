a2b8ad9473b1779ae2bb25e1bf564d4286f9147f
Broccolini <bglatman@callutheran.edu>
2019-11-13 22:56:41 -0800

    update image slides

--- client/src/app/components/ng-bootstrap/home/carousel/carousel.component.ts
+++ client/src/app/components/ng-bootstrap/home/carousel/carousel.component.ts
@@ -23,12 +23,14 @@ export class CarouselComponent implements OnInit {
     this.carouselData = [
       {
         image: this.images.C_IMG1,
-        heading: he.decode('Now Introducing a New Pok&eacute;mon ROMs Website'),
-        caption: he.decode('For All Core Pok&eacute;mon Games')
+        heading: he.decode('Pok&eacute;mon Sword and Shield Coming Soon!'),
+        caption: he.decode(
+          'The new Pok&eacute;mon games will be available on this site 1 week after their release in the US.'
+        )
       },
       {
         image: this.images.C_IMG2,
-        heading: he.decode(`Including Pok&eacute;mon Let's Go Games`),
+        heading: he.decode(`Includes Pok&eacute;mon Let's Go Games`),
         caption: he.decode(`This database includes ROMs for Pok&eacute;mon Let's Go Pikachu &amp;
         Eevee`)
       },
@@ -36,14 +38,7 @@ export class CarouselComponent implements OnInit {
         image: this.images.C_IMG3,
         heading: he.decode('Now with Pok&eacute;mon ROM Hacks'),
         caption: he.decode(
-          'You can now play your favorite Pok&eacute;mon ROM hacks'
-        )
-      },
-      {
-        image: this.images.C_IMG4,
-        heading: he.decode('Pok&eacute;mon Sword and Shield Coming Soon!'),
-        caption: he.decode(
-          'The new Pok&eacute;mon games will be available on this site 1 week after their release in the US.'
+          'You can now play some of the coolest Pok&eacute;mon ROM hacks.'
         )
       }
     ];
--- client/src/app/enums/images.enum.ts
+++ client/src/app/enums/images.enum.ts
@@ -2,7 +2,6 @@ export enum Images {
   C_IMG1 = '/assets/images/carousel_img_01.jpg',
   C_IMG2 = '/assets/images/carousel_img_02.jpg',
   C_IMG3 = '/assets/images/carousel_img_03.jpg',
-  C_IMG4 = '/assets/images/carousel_img_04.jpg',
   SPINNER = '/assets/images/spinner.gif',
   LOGO = '/assets/images/logo.png'
 }
