8827596e835b18d569c177a8afeb2a0de9b3737c
bag33188 <bglatman@callutheran.edu>
2019-11-15 09:07:55 -0800

    update spacing

--- scripts/AddLicenses.php
+++ scripts/AddLicenses.php
@@ -9,7 +9,7 @@
       $isc_license = '' .
         'ISC LICENSE' .
         "\n" .
-        "Copyright " . $current_year . " " .$name .
+        "Copyright " . $current_year . " " . $name .
         "\n" .
         'Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby ' .
         'granted, provided that the above copyright notice and this permission notice appear in all copies.' .
