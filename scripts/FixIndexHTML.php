41e375c1066a8a4524ca86847cc139d27e28797c
bag33188 <bglatman@callutheran.edu>
2019-11-15 18:05:26 -0800

    replace ' with "

--- scripts/FixIndexHTML.php
+++ scripts/FixIndexHTML.php
@@ -27,7 +27,7 @@
     protected $contents = NULL;
 
     // create constant regular expression for detecting the script tags.
-    const SCRIPT_TAG_REGEXP = '/((?:\x{003C}script src=")(?:(?:runtime|polyfills(?:-es5)?|main|vendor|scripts)(?:(?:-)?(?:(?:es(?:(?:201)?[56789]))|(?:latest)))?)(?:\.)(?:[\da-fA-F]{20})(?:\.js")(?:(?:\stype="module")?(?:\snomodule)?(?:\sdefer)?)(?:\x{003E}\x{003C}\/script\x{003E}))/';
+    const SCRIPT_TAG_REGEXP = "/((?:\x{003C}script src=\")(?:(?:runtime|polyfills(?:-es5)?|main|vendor|scripts)(?:(?:-)?(?:(?:es(?:(?:201)?[56789]))|(?:latest)))?)(?:\.)(?:[\da-fA-F]{20})(?:\.js\")(?:(?:\stype=\"module\")?(?:\snomodule)?(?:\sdefer)?)(?:\x{003E}\x{003C}\/script\x{003E}))/";
 
     /**
      * FixIndexHTML constructor.
@@ -71,23 +71,23 @@
           // loop thru script tags (0th index)
           for ($i = 0; $i < count($script_tags); $i++) {
             // if type=module is absent from script tag
-            if (strpos($script_tags[$i], 'type="module"') === false) {
+            if (strpos($script_tags[$i], "type=\"module\"") === false) {
               // add type=text/javascript
-              $script_tags[$i] = str_replace('src="', 'type="text/javascript" src="', $script_tags[$i]);
+              $script_tags[$i] = str_replace("src=\"", "type=\"text/javascript\" src=\"", $script_tags[$i]);
             }
             // if defer attribute is not present on script tag
-            if (strpos($script_tags[$i], 'defer') === false) {
+            if (strpos($script_tags[$i], "defer") === false) {
               // add the defer attribute
-              $script_tags[$i] = str_replace('></script>', ' defer></script>', $script_tags[$i]);
+              $script_tags[$i] = str_replace("></script>", " defer></script>", $script_tags[$i]);
             }
           }
 
           // move type=module to beginning of script tag and join script tags array into string (glued by newline character)
           $glued_script_tags = implode("\n", $script_tags);
           // if no type=module or type=text/javascript is detected, then add it
-          $glued_script_tags = str_replace('<script src="', '<script type="module" src="', $glued_script_tags);
+          $glued_script_tags = str_replace("<script src=\"", "<script type=\"module\" src=\"", $glued_script_tags);
           // set new script tags prop to glued script tags var
-          $this->new_script_tags = $glued_script_tags; # str_replace('type="module" ', '', $glued_script_tags);
+          $this->new_script_tags = $glued_script_tags; # str_replace("type=\"module\" ", "", $glued_script_tags);
         }
         // close the file
         fclose($file);
@@ -119,22 +119,22 @@
           // add in the script tags depending on where the main stylesheet is linked,
           // and do various checks that depend on the formatting of the file
           // if the link tag and the closing head tag on the same line
-          if ((strpos($line, '.css">') !== false || strpos($line, '<link rel="stylesheet" href="styles.') !== false) && strpos($line, '</head>') !== false) {
-            $updated_link_el_attrs = str_replace('rel="stylesheet"', 'rel="stylesheet" type="text/css"', $line);
-            $updated_head_tags = str_replace('.css"></head>', '.css" />' . "\n \n<!--[if !IE]><!-->\n" . $this->new_script_tags . "\n<!--<![endif]-->\n</head>", $updated_link_el_attrs);
+          if ((strpos($line, ".css\">") !== false || strpos($line, "<link rel=\"stylesheet\" href=\"styles.") !== false) && strpos($line, "</head>") !== false) {
+            $updated_link_el_attrs = str_replace("rel=\"stylesheet\"", "rel=\"stylesheet\" type=\"text/css\"", $line);
+            $updated_head_tags = str_replace(".css\"></head>", ".css\" />\n \n<!--[if !IE]><!-->\n" . $this->new_script_tags . "\n<!--<![endif]-->\n</head>", $updated_link_el_attrs);
             fwrite($file, $updated_head_tags . "\n");
             // if the link tag is not on the same line as the head tag
-          } elseif (strpos($line, '</head>') !== false && (strpos($line, '.css">') === false || strpos($line, '<link rel="stylesheet" href="styles.') === false)) {
-            fwrite($file, str_replace('</head>', $this->new_script_tags . "\n</head>", $line) . "\n");
+          } elseif (strpos($line, "</head>") !== false && (strpos($line, ".css\">") === false || strpos($line, "<link rel=\"stylesheet\" href=\"styles.") === false)) {
+            fwrite($file, str_replace("</head>", $this->new_script_tags . "\n</head>", $line) . "\n");
             // if the head tag is not on the same line as the link tag (different condition)
-          } elseif ((strpos($line, '.css">') !== false || strpos($line, '<link rel="stylesheet" href="styles.') !== false) and strpos($line, '</head>') === false) {
-            $updated_link_el_attrs = str_replace('rel="stylesheet"', 'rel="stylesheet" type="text/css"', $line);
-            $updated_link_tag_closed = str_replace('.css">', '.css" />' . "\n", $updated_link_el_attrs);
+          } elseif ((strpos($line, ".css\">") !== false || strpos($line, "<link rel=\"stylesheet\" href=\"styles.") !== false) and strpos($line, "</head>") === false) {
+            $updated_link_el_attrs = str_replace("rel=\"stylesheet\"", "rel=\"stylesheet\" type=\"text/css\"", $line);
+            $updated_link_tag_closed = str_replace(".css\">", ".css\" />\n", $updated_link_el_attrs);
             fwrite($file, $updated_link_tag_closed . "\n");
             // check if script tags are in current line
           } elseif ($script_tags_exist === true) {
             foreach ($script_tags[0] as $script_tag) {
-              $line = str_replace($script_tag, '', $line);
+              $line = str_replace($script_tag, "", $line);
             }
             fwrite($file, $line . "\n");
           } else {
@@ -162,12 +162,12 @@
       // encapsulate main logic in try-catch (in case of errors)
       try {
         // define constant if not already defined
-        if (!defined('IMPORTANT_COMMENT')) {
-          define('IMPORTANT_COMMENT', '<!-- May the source be with you! -->');
+        if (!defined("IMPORTANT_COMMENT")) {
+          define("IMPORTANT_COMMENT", "<!-- May the source be with you! -->");
         }
         // create file vars
         // open file for writing
-        $file = fopen($this->filepath, 'w');
+        $file = fopen($this->filepath, "w");
         // create file lines var by splitting contents by newline char
         $file_lines = explode("\n", $this->contents);
         // define search and replace string vars
@@ -216,7 +216,7 @@
         // set contents prop to text in file
         $this->contents = file_get_contents($this->filepath);
         // replace trailing newlines with blank string
-        $this->contents = preg_replace('/\n+$/m', '', $this->contents);
+        $this->contents = preg_replace("/\n+$/m", "", $this->contents);
       } catch (Exception $e) {
         // handle any exceptions
         echo $e->getMessage();
@@ -231,14 +231,14 @@
      */
     private static function strip($string) {
       // define regexp constants if not already defined
-      if (!defined('LEFT_TRIM')) {
-        define('LEFT_TRIM', '/^[\s\t]+]/');
+      if (!defined("LEFT_TRIM")) {
+        define("LEFT_TRIM", "/^[\s\t]+]/");
       }
-      if (!defined('RIGHT_TRIM')) {
-        define('RIGHT_TRIM', '/[\s\t]+$/');
+      if (!defined("RIGHT_TRIM")) {
+        define("RIGHT_TRIM", "/[\s\t]+$/");
       }
       // replace left and right leading/trailing whitespace and tabs on string with nothing.
-      $stripped_string = preg_replace(LEFT_TRIM, '', preg_replace(RIGHT_TRIM, '', $string));
+      $stripped_string = preg_replace(LEFT_TRIM, "", preg_replace(RIGHT_TRIM, "", $string));
       // return stripped value
       return $stripped_string;
     }
@@ -249,7 +249,7 @@
    */
   function main() {
     // instantiate class and pass in path to `index.html` file
-    $fix_index_html = new FixIndexHTML('../public/index.html');
+    $fix_index_html = new FixIndexHTML("../public/index.html");
     // begin fixing index.html file ...
     echo "Moving around script tags in index.html ... \n";
     $fix_index_html->read_script_tags();
