19c6a263a048585bc9ae6971f67f3d2cac860942
bag33188 <bglatman@callutheran.edu>
2019-11-15 17:04:22 -0800

    fix

--- www/metadata.inc.php
+++ www/metadata.inc.php
@@ -23,7 +23,7 @@
               }
             </style>
             <script type='text/javascript'>
-              console.log('Error 404: Page not found.');
+              console.error('Error 404: Page not found.');
             </script>
           </head>
           <body style='margin:0;font-family:-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,\"Helvetica Neue\",Arial,\"Noto Sans\",sans-serif,\"Apple Color Emoji\",\"Segoe UI Emoji\",\"Segoe UI Symbol\",\"Noto Color Emoji\";font-size:1rem;font-weight:400;line-height:1.5;color:#212529;text-align:left;background-color:#fff;padding:1rem!important;'>
