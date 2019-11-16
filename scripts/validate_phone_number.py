3287c25d47d6753480848e6a2636bd17f5da58bc
bag33188 <bglatman@callutheran.edu>
2019-11-15 21:45:05 -0800

    fix

--- scripts/validate_phone_number.py
+++ scripts/validate_phone_number.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 
+"""
+This program validates any given United States phone number.
+"""
+
+# print(__doc__)
+
 import re
 
 def validate_phone_number(phone_number):
+  """
+  Validates a US phone number based on a complex regular expression.
+  """
+  # print(validate_phone_number.__doc__)
   phone_number_regexp = '^(?:([2-9]1{2})|(?:(\+?1\s*(?:[.-]\s*)?)?(?:\(?\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)?)\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(\x20+|#|x\.?|ext\.?|extension)\s*(\d+))?)$'
   phone_number_match = re.match(phone_number_regexp, phone_number, re.IGNORECASE)
   return False if phone_number_match == None else True
 
 def main():
+  """
+  Asks the user for a phone number, then tells them whether it is valid or not.
+  """
+  # print(main.__doc__)
   while True:
     phone_number = input('Please enter a phone number: ')
     if phone_number == '' or len(phone_number) == 0:
