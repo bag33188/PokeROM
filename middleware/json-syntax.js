5f36ddf31782503d2de01b1dd4a1fe5e6bfd2ee9
bag33188 <bglatman@callutheran.edu>
2019-11-17 09:29:18 -0800

    fix

--- middleware/json-syntax.js
+++ middleware/json-syntax.js
@@ -2,6 +2,8 @@ function jsonSyntax(err, req, res, next) {
   if (err instanceof SyntaxError) {
     if (err.status === 400) {
       res.status(400).json({ success: false, message: 'Invalid JSON.' });
+    } else {
+      return;
     }
   } else {
     next();
