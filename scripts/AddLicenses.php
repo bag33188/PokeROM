e13cebb1b87d589433da808b8d21fe6f4ff90724
bag33188 <bglatman@callutheran.edu>
2019-11-17 11:08:42 -0800

    update code base

--- scripts/AddLicenses.php
+++ scripts/AddLicenses.php
@@ -12,7 +12,7 @@
   class AddLicenses {
 
     // create name constant (class property)
-    const NAME = "Broccolini";
+    const APP_NAME = "PokeROM";
 
     /**
      * AddLicenses constructor.
@@ -20,77 +20,44 @@
     public function __construct()
     {
       // throw error if new instance of this class is created.
-      throw new Error("Error: AddLicenses class is not meant to be instantiated.");
+      throw new Error("Error: The `AddLicenses` class is not meant to be instantiated.");
     }
 
     /**
-     * @param string $name Copyright owner's name.
      * @return string ISC License.
      */
-    private static function get_isc_license($name) {
-      // store current year in var
-      $current_year = date("Y");
-      // create empty-str var for isc license
-      $isc_license = "";
-      // isc license data
-      $isc_license_data = array(
-        "ISC LICENSE",
-        "\n",
-        "Copyright " . $current_year . " " . $name,
-        "\n",
-        "Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby ",
-        "granted, provided that the above copyright notice and this permission notice appear in all copies.",
-        "\n",
-        "THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ",
-        "ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, ",
-        "DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR ",
-        "PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION ",
-        "WITH THE USE OR PERFORMANCE OF THIS SOFTWARE."
-      );
-      // loop thru license data
-      for ($i = 0; $i < count($isc_license_data); $i++) {
-        // append to str var
-        $isc_license .= $isc_license_data[$i];
+    private static function get_isc_license() {
+      $isc_license = NULL;
+      try {
+        $filename = "../LICENSE";
+        $file = fopen($filename, "r");
+        $filesize = filesize($filename);
+        $content = fread($file, $filesize);
+        fclose($file);
+        $isc_license = "ISC LICENSE\n" . str_replace("\n\n", "\n", $content);
+      } catch (Exception $e) {
+        echo "Error:\nCode: " . strval($e->getCode()) . "\nMessage: " . $e->getMessage();
+        die(1);
       }
-      // return isc license
       return $isc_license;
     }
 
     /**
-     * @param string $name Copyright owner's name.
      * @return string MIT License.
      */
-    private static function get_mit_license($name) {
-      // store current year in var
-      $current_year = date("Y");
-      // create empty-str var for mit license
-      $mit_license = "";
-      // mit license data
-      $mit_license_data = array(
-        "MIT LICENSE",
-        "\n",
-        "Copyright " . $current_year . " " . $name,
-        "\n",
-        "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated ",
-        "documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation ",
-        "the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and",
-        " to permit persons to whom the Software is furnished to do so, subject to the following conditions:",
-        "\n",
-        "The above copyright notice and this permission notice shall be included in all copies or substantial portions ",
-        "of the Software.",
-        "\n",
-        "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED ",
-        "TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. ",
-        "IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, ",
-        "WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR ",
-        "THE USE OR OTHER DEALINGS IN THE SOFTWARE."
-      );
-      // loop thru license data
-      for ($i = 0; $i < count($mit_license_data); $i++) {
-        // append to str var
-        $mit_license .= $mit_license_data[$i];
+    private static function get_mit_license() {
+      $mit_license = NULL;
+      try {
+        $filename = "../client/LICENSE";
+        $file = fopen($filename, "r");
+        $filesize = filesize($filename);
+        $content = fread($file, $filesize);
+        fclose($file);
+        $mit_license = "MIT LICENSE\n" . str_replace("\n\n", "\n", $content);
+      } catch (Exception $e) {
+        echo "Error:\nCode: " . strval($e->getCode()) . "\nMessage: " . $e->getMessage();
+        die(1);
       }
-      // return mit license
       return $mit_license;
     }
 
@@ -106,18 +73,17 @@
         $licenses = "";
         // store license data in var
         $license_data = array(
-          "pokerom",
+          strtolower(self::APP_NAME),
           "\n",
           "ISC",
           "\n",
           "MIT",
           "\n\n",
           // get isc license (pass in name const)
-          self::get_isc_license(self::NAME),
+          self::get_isc_license(),
           "\n\n",
           // get mit license (pass in name const)
-          self::get_mit_license(self::NAME),
-          "\n"
+          self::get_mit_license()
         );
         // loop thru license data
         foreach ($license_data as $data) {
@@ -128,6 +94,8 @@
         $file = fopen($filepath, "w");
         // write licenses text to new file
         fwrite($file, $licenses);
+        // close file
+        fclose($file);
         echo "Done!\n\n";
       } catch (Exception $e) {
         // catch any exceptions
@@ -147,6 +115,7 @@
       define("FILEPATH", "../public/LICENSE");
     }
     // call `write_file` method from `AddLicenses` class (passing in `FILEPATH` constant)
+    # AddLicenses::write_file(FILEPATH);
     AddLicenses::write_file(FILEPATH);
   }
 
