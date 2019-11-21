f6768ada6c3ab61f6e6a9fd09ef5b1ae238373d5
bag33188 <bglatman@callutheran.edu>
2019-11-20 18:04:39 -0800

    fix

--- controllers/options-controller.js
+++ controllers/options-controller.js
@@ -1,23 +1,7 @@
-/**
- * @function
- * @name options
- * @description Options request.
- * @param {{}} req
- * @param {{}} res
- * @returns {void} Nothing (no content).
- */
 module.exports.options = (req, res) => {
   res.status(204);
 };
 
-/**
- * @function
- * @name all
- * @description All methods.
- * @param {{}} req The request object.
- * @param {{}} res The response object.
- * @returns {*|Format|Promise<any>} An error JSON response.
- */
 module.exports.all = (req, res) => {
   const methods = ['OPTIONS', 'HEAD'];
   if (methods.includes(req.method)) {
