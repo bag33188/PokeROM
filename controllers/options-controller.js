d7097d6ee9a3201300b01eaec851c8ada402190e
bag33188 <bglatman@callutheran.edu>
2019-11-22 09:14:35 -0800

    fix

--- controllers/options-controller.js
+++ controllers/options-controller.js
@@ -3,8 +3,8 @@ const url = require('url');
 module.exports.options = (req, res) => {
   const baseUrl =
     process.env.NODE_ENV === 'production'
-      ? 'https://www.pokerom.dev'
-      : 'http://localhost:4200';
+      ? 'https://www.pokerom.dev/'
+      : 'http://localhost:4200/';
   res.cookie('base-url', url.parse(baseUrl));
   res.status(204);
 };
