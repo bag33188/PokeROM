93ef50a8c7b7c412571a22946279062a85b5f497
bag33188 <bglatman@callutheran.edu>
2019-11-24 12:52:33 -0800

    fix doc

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,38 +1,36 @@
 const express = require('express');
 const yaml = require('js-yaml');
 const fs = require('fs');
-
-const naturesDoc = yaml.safeLoad(fs.readFileSync('./docs/paths/Natures.yml'));
-const ratingsDoc = yaml.safeLoad(fs.readFileSync('./docs/paths/Ratings.yml'));
-const usersDoc = yaml.safeLoad(fs.readFileSync('./docs/paths/Users.yml'));
-const optionsDoc = yaml.safeLoad(fs.readFileSync('./docs/paths/Options.yml'));
-const romsDoc = yaml.safeLoad(fs.readFileSync('./docs/paths/Roms.yml'));
-const versionDoc = yaml.safeLoad(fs.readFileSync('./docs/paths/Version.yml'));
+const [, version] = require('../../docs/swagger-doc');
 
 const docs = [
-  naturesDoc,
-  ratingsDoc,
-  romsDoc,
-  optionsDoc,
-  usersDoc,
-  versionDoc
+  yaml.safeLoad(fs.readFileSync('./docs/paths/Roms.yml')),
+  yaml.safeLoad(fs.readFileSync('./docs/paths/Users.yml')),
+  yaml.safeLoad(fs.readFileSync('./docs/paths/Natures.yml')),
+  yaml.safeLoad(fs.readFileSync('./docs/paths/Ratings.yml')),
+  yaml.safeLoad(fs.readFileSync('./docs/paths/Options.yml')),
+  yaml.safeLoad(fs.readFileSync('./docs/paths/Version.yml'))
 ];
 
 let pathsArr = [];
 
-docs.forEach(doc => {
-  pathsArr.push(Object.keys(doc.paths));
-});
+docs.forEach(doc => pathsArr.push(Object.keys(doc.paths)));
 
 pathsArr = pathsArr.flat(1).sort();
 
 let listItems = '';
 
-for (path of pathsArr) {
-  listItems += `<li style="list-style-type: none;"><code>${path}</code></li>\n`;
+for (const i in pathsArr) {
+  const path = pathsArr[+i];
+  listItems += `<li style="list-style-type: none;"><code>${path}</code></li>${
+    +i === pathsArr.length - 1 ? '' : '\n'
+  }`;
 }
 
-listItems = listItems.replace(/(\n)$/, '');
+const apiDocsHtml =
+  process.env.NODE_ENV !== 'production'
+    ? `<hr /><a href="/api/docs/${version}" target="_blank">API Docs</a>`
+    : '';
 
 let htmlDoc = `
 <!DOCTYPE html>
@@ -41,32 +39,78 @@ let htmlDoc = `
     <meta charset="UTF-8" />
     <title>Pok&eacute;ROM API</title>
     <style type="text/css">
+      :root {
+        --plastic-pink: #e83e8c;
+        --almost-black: #212529;
+        --white: #fff;
+        --anchor-blue: #007bff;
+        --anchor-hover-blue: #0056b3;
+      }
+      *,
+      *::before,
+      *::after {
+        box-sizing: border-box;
+      }
       body {
-        font-family: Roboto, Arial, Verdana, sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+          font-weight: 400;
+          line-height: 1.5;
+          font-size: 1rem;
+          color: var(--almost-black);
+          background-color: var(--white);
       }
       #heading-title {
         margin-top: 2rem;
+        font-weight: 500;
+        font-size: 2.5rem;
+        margin-bottom: 0;
       }
       .container-wrapper {
         text-align: center;
       }
+      code {
+        font-family: SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+        font-size: 1em;
+        color: var(--plastic-pink);
+        word-break: break-word;
+      }
+      a {
+        color: var(--anchor-blue);
+        text-decoration: none;
+        background-color: transparent;
+      }
+      a:hover {
+        color: var(--anchor-hover-blue);
+        text-decoration: underline;
+      }
+      hr {
+        box-sizing: content-box !important;
+        height: 0;
+        overflow: visible;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+        border: 0;
+        border-top: 1px solid rgba(0, 0, 0, 0.1);
+      }
     </style>
     <script type="text/javascript">
-      console.error('Please use a valid Pok\u00E9ROM API endpoint.');
+      const eacute = '\u00E9';
+      console.error(\`Please use a valid Pok\$\{eacute\}ROM API endpoint.\`);
     </script>
   </head>
   <body>
     <div class="container-wrapper">
-      <h1 id="heading-title">Please use a valid Pok&eacute;ROM API endpoint.</h1>
-      <ul>
+      <h1 id="heading-title">Please use a valid Pok&eacute;ROM API endpoint:</h1>
+      <ul style="padding: 0;">
         ${listItems}
       </ul>
+      ${apiDocsHtml}
     </div>
   </body>
 </html>
 `;
 
-htmlDoc = htmlDoc.replace(/^(\n)/, '');
+htmlDoc = htmlDoc.replace(/^(\n)/, '').replace(/(\n)$/, '');
 
 const router = express.Router();
 
