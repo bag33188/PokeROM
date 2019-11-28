bc76de08c52b5634a95ce7972010d418040be14c
Broccolini <bglatman@callutheran.edu>
2019-11-27 16:31:15 -0800

    update code base

--- client/src/app/components/roms/update-roms/update-roms.component.ts
+++ client/src/app/components/roms/update-roms/update-roms.component.ts
@@ -6,8 +6,10 @@ import { JSONObject } from '../../../models/JSONObject';
 import { Rom } from '../../../models/Rom';
 import { LoggerService as logger } from '../../../services/logger.service';
 import { IconDefinition } from '@fortawesome/free-brands-svg-icons';
-import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCheckCircle,
+  faTimesCircle
+} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-update-roms',
@@ -19,15 +21,16 @@ export class UpdateRomsComponent implements OnInit, OnDestroy {
   private addRomsObs$: Observable<[Rom[], Rom[]]>;
   private addRomsSub: Subscription;
   private deleteRomsSub: Subscription;
-  public romsUpdated: boolean;
+  public romsUpdated: boolean | undefined;
   public loading: boolean;
   public showBtn: boolean;
   public faCheckCircle: IconDefinition;
-  public faExclamationTriangle: IconDefinition;
+  public faTimesCircle: IconDefinition;
   constructor(private romsService: RomsService) {}
 
   ngOnInit(): void {
-    this.faExclamationTriangle = faExclamationTriangle;
+    this.romsUpdated = undefined;
+    this.faTimesCircle = faTimesCircle;
     this.faCheckCircle = faCheckCircle;
     this.showBtn = true;
     this.loading = false;
@@ -55,6 +58,7 @@ export class UpdateRomsComponent implements OnInit, OnDestroy {
       (err: JSONObject): void => {
         this.loading = false;
         deleteSuccess = false;
+        this.romsUpdated = deleteSuccess;
         logger.error(err);
       },
       (): void => {
