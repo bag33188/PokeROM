472258e43fb993bb77520f20d3012fe75d202434
Broccolini <bglatman@callutheran.edu>
2019-11-29 18:06:03 -0800

    update code base

--- client/src/app/components/ng-bootstrap/home/splash-carousel/splash-carousel.component.ts
+++ client/src/app/components/ng-bootstrap/home/splash-carousel/splash-carousel.component.ts
@@ -26,21 +26,21 @@ export class SplashCarouselComponent implements OnInit {
         heading: `Pok${SplashCarouselComponent.eacute}mon Sword and Shield now available!`,
         caption: `The new Pok${SplashCarouselComponent.eacute}mon games are now available on this website.`
       },
-      // {
-      //   image: this.images.C_IMG2,
-      //   heading: `Includes Pok${CarouselComponent.eacute}mon Let's Go Games`,
-      //   caption: `This database includes ROMs for Pok${CarouselComponent.eacute}mon Let's Go Pikachu & Eevee`
-      // },
+      {
+        image: this.images.C_IMG2,
+        heading: `Includes Pok${SplashCarouselComponent.eacute}mon Let's Go Games`,
+        caption: `This database includes ROMs for Pok${SplashCarouselComponent.eacute}mon Let's Go Pikachu & Eevee`
+      }
       // {
       //   image: this.images.C_IMG3,
-      //   heading: `Now with Pok${CarouselComponent.eacute}mon ROM Hacks`,
-      //   caption: `You can now play some of the coolest Pok${CarouselComponent.eacute}mon ROM hacks.`
+      //   heading: `Now with Pok${SplashCarouselComponent.eacute}mon ROM Hacks`,
+      //   caption: `You can now play some of the coolest Pok${SplashCarouselComponent.eacute}mon ROM hacks.`
       // },
-      {
-        image: this.images.C_IMG4,
-        heading: `Grab all your core Pok${SplashCarouselComponent.eacute}mon Game ROMs here!`,
-        caption: `This website's database houses all the core Pok${SplashCarouselComponent.eacute}mon game ROMs ever made.`
-      }
+      // {
+      //   image: this.images.C_IMG4,
+      //   heading: `Grab all your core Pok${SplashCarouselComponent.eacute}mon Game ROMs here!`,
+      //   caption: `This website's database houses all the core Pok${SplashCarouselComponent.eacute}mon game ROMs ever made.`
+      // }
     ];
   }
   public getInterval(): number {
