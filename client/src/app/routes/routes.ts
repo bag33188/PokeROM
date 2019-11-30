5bd52a84cde5bce9b6a63a001eb21b22ebc1ef7e
Broccolini <bglatman@callutheran.edu>
2019-11-29 18:33:20 -0800

    jnk

--- client/src/app/routes/routes.ts
+++ client/src/app/routes/routes.ts
@@ -1,13 +1,29 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '../guards/auth.guard';
 import { NotFoundComponent } from '../components/pages/not-found/not-found.component';
+import { HomeComponent } from '../components/pages/home/home.component';
+import { LoginComponent } from '../components/pages/login/login.component';
+import { RomsComponent } from '../components/pages/roms/roms.component';
+import { RomInfoComponent } from '../components/pages/rom-info/rom-info.component';
+import { FaqComponent } from '../components/pages/faq/faq.component';
+import { NaturesComponent } from '../components/pages/natures/natures.component';
+import { RatingsComponent } from '../components/pages/ratings/ratings.component';
 import { IsSecureGuard } from '../guards/is-secure.guard';
+import { RegisterComponent } from '../components/pages/register/register.component';
+import { AccountComponent } from '../components/pages/account/account.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/',
+    redirectTo: '/home',
     pathMatch: 'full',
     canActivate: [IsSecureGuard]
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [IsSecureGuard],
+    loadChildren: 'app/components/pages/pages.module#PagesModule'
   },
   { path: '**', redirectTo: '/not_found', canActivate: [IsSecureGuard] },
   {
