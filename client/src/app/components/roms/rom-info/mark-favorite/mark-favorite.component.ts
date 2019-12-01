1af53c0ae4dacf7e5e7c6510414f6902cb0d5ff4
Broccolini <bglatman@callutheran.edu>
2019-12-01 04:31:31 -0800

    update code base

--- client/src/app/components/roms/rom-info/mark-favorite/mark-favorite.component.ts
+++ client/src/app/components/roms/rom-info/mark-favorite/mark-favorite.component.ts
@@ -14,9 +14,12 @@ export class MarkFavoriteComponent implements OnInit {
   @Input() public isFavorite: boolean;
   @Input() private id: string;
   public faStar: IconDefinition;
+  public label: string;
+
   constructor(private romsService: RomsService) {}
 
   public ngOnInit(): void {
+    this.label = 'Mark as Favorite';
     this.faStar = faStar;
   }
 
@@ -24,19 +27,20 @@ export class MarkFavoriteComponent implements OnInit {
     const partialRom: { is_favorite: boolean } = {
       is_favorite: !this.isFavorite
     };
+    // toggle on UI
     this.isFavorite = partialRom.is_favorite;
+    // toggle on server
     this.romsService
       // tslint:disable-next-line:whitespace
       .patchRom(this.id, (<unknown>partialRom) as JSONObject)
       .subscribe(
         (res: Rom): void => {
+          // check for UI/server mismatch
           if (this.isFavorite !== res.is_favorite) {
             this.isFavorite = res.is_favorite;
           }
         },
-        (err: JSONObject): void => {
-          logger.error(err);
-        }
+        (err: JSONObject): void => logger.error(err)
       );
   }
 }
