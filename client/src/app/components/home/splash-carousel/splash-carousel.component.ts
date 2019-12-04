8637ea7af1b54c3b3e7cc29ee6c74351b93eebd2
bag33188 <bglatman@callutheran.edu>
2019-12-04 10:24:54 -0800

    fix

--- client/src/app/components/home/splash-carousel/splash-carousel.component.ts
+++ client/src/app/components/home/splash-carousel/splash-carousel.component.ts
@@ -25,17 +25,17 @@ export class SplashCarouselComponent implements OnInit {
         image: this.images.C_IMG1,
         heading: `Pok${SplashCarouselComponent.eacute}mon Sword and Shield now available!`,
         caption: `The new Pok${SplashCarouselComponent.eacute}mon games are now available on this website.`
-      },
-      {
-        image: this.images.C_IMG2,
-        heading: `Includes Pok${SplashCarouselComponent.eacute}mon Let's Go Games`,
-        caption: `This database includes ROMs for Pok${SplashCarouselComponent.eacute}mon Let's Go Pikachu & Eevee`
       }
+      // {
+      //   image: this.images.C_IMG2,
+      //   heading: `Includes Pok${SplashCarouselComponent.eacute}mon Let's Go Games`,
+      //   caption: `This database includes ROMs for Pok${SplashCarouselComponent.eacute}mon Let's Go Pikachu & Eevee`
+      // },
       // {
       //   image: this.images.C_IMG3,
       //   heading: `Now with Pok${SplashCarouselComponent.eacute}mon ROM Hacks`,
       //   caption: `You can now play some of the coolest Pok${SplashCarouselComponent.eacute}mon ROM hacks.`
-      // }
+      // },
       // {
       //   image: this.images.C_IMG4,
       //   heading: `Grab all your core Pok${SplashCarouselComponent.eacute}mon Game ROMs here!`,
