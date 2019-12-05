d719b284907f985e07b24444407a7eff109a236f
Broccolini <bglatman@callutheran.edu>
2019-12-05 02:10:03 -0800

    fix

--- controllers/option-controller.js
+++ controllers/option-controller.js
@@ -1,12 +1,6 @@
-const url = require('url');
 const universal = require('../routes/universal');
 
 module.exports.options = (req, res) => {
-  const baseUrl =
-    process.env.NODE_ENV === 'production'
-      ? 'https://www.pokerom.dev/'
-      : 'http://localhost:4200/';
-  res.cookie('base-url', url.parse(baseUrl));
   res.status(204);
 };
 
