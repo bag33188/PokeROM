294ed62c79627e9d30d1d693a73e556338e15a47
Broccolini <bglatman@callutheran.edu>
2019-12-07 02:07:55 -0800

    fix

--- www/js/ready.js
+++ www/js/ready.js
@@ -1,62 +1,52 @@
+/**
+ * @callback callback
+ * @returns {void} Nothing.
+ */
+
+/**
+ * @var
+ * @class
+ * @name $
+ * @summary Document ready function
+ * @description Waits for the page to load before executing main code.
+ * @param {Document} documentObject The browser's document object or a callback function.
+ * @returns {void | { ready: ready }} An object literal with a ready property or void (nothing).
+ */
 var $ = function(documentObject) {
   try {
+    /**
+     * @var
+     * @function
+     * @name ready
+     * @summary Ready function.
+     * @description Waits for page to finish loading, then executes callback function.
+     * @param {callback | Document} callback The callback function to execute after page loads.
+     * @returns {void} Nothing.
+     */
+    var ready = function(callback) {
+      if (documentObject.readyState !== 'loading') {
+        callback();
+      } else if (
+        documentObject.addEventListener !== null &&
+        documentObject.addEventListener !== undefined
+      ) {
+        documentObject.addEventListener('DOMContentLoaded', callback, false);
+      } else {
+        documentObject.attachEvent('onreadystatechange', function() {
+          if (documentObject.readyState === 'complete') {
+            callback();
+          }
+        });
+      }
+    };
     if (typeof documentObject === 'object') {
       return {
-        ready: function(callback) {
-          if (documentObject.readyState !== 'loading') {
-            callback();
-          } else if (
-            documentObject.addEventListener !== null &&
-            documentObject.addEventListener !== undefined
-          ) {
-            documentObject.addEventListener(
-              'DOMContentLoaded',
-              callback,
-              false
-            );
-          } else {
-            documentObject.attachEvent('onreadystatechange', function() {
-              if (documentObject.readyState === 'complete') {
-                callback();
-              }
-            });
-          }
-        }
+        ready: ready
       };
     } else {
-      (function ready(callback) {
-        if (document.readyState !== 'loading') {
-          callback();
-        } else if (
-          document.addEventListener !== null &&
-          document.addEventListener !== undefined
-        ) {
-          document.addEventListener('DOMContentLoaded', callback, false);
-        } else {
-          document.attachEvent('onreadystatechange', function() {
-            if (document.readyState === 'complete') {
-              callback();
-            }
-          });
-        }
-      })(documentObject);
+      ready(documentObject);
     }
   } catch (err) {
     throw err;
   }
 };
-
-/*
-how to use
-----------
-
-method 1 (older browsers):
-$(document).ready(function() {
-  // code goes here
-});
-
-method 2 (modern browsers):
-$(function() {
-  // code goes here
-});
-*/
