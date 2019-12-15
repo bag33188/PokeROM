92b7de33fa467e17bfa5a122bc8730c91fc97530
Broccolini <bglatman@callutheran.edu>
2019-12-14 19:42:14 -0800

    fix

--- www/js/ready.js
+++ www/js/ready.js
@@ -1,28 +1,5 @@
-/**
- * @callback callback
- * @returns {void} Nothing.
- */
-
-/**
- * @var
- * @class
- * @name $
- * @summary Document ready function
- * @description Waits for the page to load before executing main code.
- * @param {Document} documentObject The browser's document object or a callback function.
- * @returns {void | { ready: ready }} An object literal with a ready property or void (nothing).
- */
 var $ = function(documentObject) {
   try {
-    /**
-     * @var
-     * @function
-     * @name ready
-     * @summary Ready function.
-     * @description Waits for page to finish loading, then executes callback function.
-     * @param {callback | Document} callback The callback function to execute after page loads.
-     * @returns {void} Nothing.
-     */
     var ready = function(callback) {
       if (documentObject.readyState !== 'loading') {
         callback();
