f1193f6a55b66edc9f0a1973d89c2e55a5cc8ef8
bag33188 <bglatman@outlook.com>
2020-06-03 08:40:14 -0700

    update certs

--- config/certs.js
+++ config/certs.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const certificate = fs
   .readFileSync('database/mongodb.pem', 'utf8')
   .split('\n')
-  .slice(0, 35)
+  .slice(0, 34)
   .join('\n');
 const certificateKey = fs
   .readFileSync('database/mongodb.pem', 'utf8')
   .split('\n')
-  .splice(35, 61)
+  .splice(34, 61)
   .join('\n');
 const certificateAuthority = fs.readFileSync('database/mongodb.crt', 'utf8');
 
