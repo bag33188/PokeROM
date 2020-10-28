99717b4d5f533654f0bff83f437b56ce76fe9360
bag33188 <bglatman@outlook.com>
2020-10-28 13:26:15 -0700

    add resources batch file

---
+++ misc/resources.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+
+:RESOURCES
+	START "" https://angular.io/
+	TIMEOUT /T 1
+	START "" https://cli.angular.io/
+	TIMEOUT /T 1
+	START "" https://ngrx.io/guide/store
+	TIMEOUT /T 1
+	START "" https://rxjs-dev.firebaseapp.com/guide/overview
+	TIMEOUT /T 1
+	START "" https://ng-bootstrap.github.io/#/home
+	TIMEOUT /T 1
+	START "" https://www.typescriptlang.org/docs/handbook/declaration-merging.html
+	TIMEOUT /T 1
+	START "" https://sass-lang.com/documentation/interpolation
+	TIMEOUT /T 1
+	START "" https://docs.mongodb.com/manual/
+	TIMEOUT /T 1
+	START "" https://nodejs.org/api/os.html#os_os_loadavg
+	TIMEOUT /T 1
+	START "" "https://www.php.net/manual/en/refs.database.php"
+	TIMEOUT /T 1
+	START "" https://regexr.com/
+	TIMEOUT /T 1
+	START "" https://cloud.mongodb.com/
+	TIMEOUT /T 1
+	START "" https://www.udemy.com/home/my-courses/learning/
+	TIMEOUT /T 1
+	START "" https://youtube.com/techguyweb
+	TIMEOUT /T 1
+	START "" "https://www.iheart.com/podcast/966-the-angular-show-59795034/?cmp=ios_share&sc=ios_social_share&pr=false&autoplay=true"
+EXIT /B 0
